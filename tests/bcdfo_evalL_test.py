f30fc7353835aae71218ef6b412e58bbf1800fa3
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-08 12:58:04 +0200

    Addition of new tests and comments New tests include test of classic proprieties on lagrange polynomials. Comments quickly explains the important points we are testing.

--- tests/bcdfo_evalL_test.py
+++ tests/bcdfo_evalL_test.py
@@ -7,50 +7,88 @@ Created on Tue Dec 02 14:54:04 2014
 
 import unittest
 from bcdfo_evalL import bcdfo_evalL_
-from bcdfo_build_QR_of_Y import bcdfo_build_QR_of_Y_, bcdfo_build_QR_of_Y
-from runtime import matlabarray
-import numpy as np
+from bcdfo_build_QR_of_Y import bcdfo_build_QR_of_Y_
+from runtime import matlabarray, compare_matlabarray
+from random import random
 #import helper
 
 class Test_bcdfo_evalL(unittest.TestCase):
+    """
+    #Reminder :
+    #This class is a test for evalL which compute the values of the lagrange polynomials l_j(x), such that the output vector values is :
+    #[l_1(x), l_2(x), l_3(x),....l_p(x)] where p is the number of points of the interpolant set.
+    """ 
+    def setUp(self):
+        #self.options = helper.dummyOptions()
+        #self.values = helper.dummyValues()
+           self.abs_tol=1e-2;
+           self.rel_tol=1e-2;
+           pass
 
-	def setUp(self):
-		#self.options = helper.dummyOptions()
-		#self.values = helper.dummyValues()
+    #We verify here that for x being a point of the interpolant set, we have l_j(x) = 1 if the index j corresponds to the index of x in the interpolant set
+    #and 0 otherwise
+    def test_bcdfo_evalL_1(self):
+        Y = matlabarray([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
+        QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y, 0, 1, 1,1,1e15 )
+        
+        #Here we choose the first point of the interpolant set
+        values = bcdfo_evalL_( QZ, RZ, Y, matlabarray(range(0,6)), matlabarray([[0],[0]]), xbase, 0, scale, 1 )
+        correctvalues = matlabarray([ [1],   [0],    [0],    [0], [0],   [0] ])
+        
+        self.assertTrue(compare_matlabarray(values, correctvalues, self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(xbase, matlabarray([0,0]), self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(scale, matlabarray([1,0.5, 0.5, 0.25, 0.25, 0.25]), self.abs_tol, self.rel_tol))
+        
+    #We verify here that for x being a point of the interpolant set, we have l_j(x) = 1 if the index j corresponds to the index of x in the interpolant set
+    #and 0 otherwise
+    def test_bcdfo_evalL_2(self):
+        Y = matlabarray([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
+        QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y, 0, 1, 1,1,1e15 )
+        
+        #Here we choose the fifth point of the interpolant set
+        values = bcdfo_evalL_( QZ, RZ, Y, matlabarray(range(1,6)), matlabarray([[1],[0.01]]), xbase, 0, scale, 1 )
+        correctvalues = matlabarray([ [0.],   [0.],    [0.],    [0.], [1.],   [0.] ])
+        
+        self.assertTrue(compare_matlabarray(values, correctvalues, self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(xbase, matlabarray([0,0]), self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(scale, matlabarray([1,0.5, 0.5, 0.25, 0.25, 0.25]), self.abs_tol, self.rel_tol))
+        
+    #We verify here that for x being a point of the interpolant set, we have l_j(x) = 1 if the index j corresponds to the index of x in the interpolant set
+    #and 0 otherwise
+    def test_bcdfo_evalL_3(self):
+        Y = matlabarray([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
+        QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y, 0, 1, 1,1,1e15 )
+        
+        #Here we choose the fourth point of the interpolant set
+        values = bcdfo_evalL_( QZ, RZ, Y, matlabarray([2, 3, 5]), matlabarray([[2],[0]]), xbase, 0, scale, 1 )
+        correctvalues = matlabarray([ [0],   [0],    [0],  [1], [0],   [0] ])
+        
+        self.assertTrue(compare_matlabarray(values, correctvalues, self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(xbase, matlabarray([0,0]), self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(scale, matlabarray([1,0.5, 0.5, 0.25, 0.25, 0.25]), self.abs_tol, self.rel_tol))
 
-		pass
+    #We verify here that for x random, the sum of the values is equal to 1 when we have a enough point to have a complete basis.
+    def test_bcdfo_evalL_4(self):
+        Y = matlabarray([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
+        QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y, 0, 1, 1,1,1e15 )
+        
+        #Here we choose a random point, and the complete interpolant set
+        values = bcdfo_evalL_( QZ, RZ, Y, matlabarray(range(0,6)), matlabarray([[random()],[random()]]), xbase, 0, scale, 1 )
+        self.assertAlmostEqual(values.sum(), 1.0,places=13)
+        self.assertTrue(compare_matlabarray(xbase, matlabarray([0,0]), self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(scale, matlabarray([1,0.5, 0.5, 0.25, 0.25, 0.25]), self.abs_tol, self.rel_tol))
 
-	def test_bcdfo_evalL(self):
-		Y = matlabarray([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
-		QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y, 0, 1, 1,1,1e15 )
-		values = bcdfo_evalL_( QZ, RZ, Y, matlabarray(range(1,6)), matlabarray([[-1],[1]]), xbase, 0, scale, 1 )
-		#print "values", values
-		correctvalues = matlabarray([ [0],   [97.0000],    [2.9900],    [1.0000], [-100.0000],   [-0.4950] ])
-		self.assertTrue((abs(values - correctvalues) < 1e-8).all())
-		
-	def est_bcdfo_evalLOriginal(self):
-		Y = np.array([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
-		QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y( Y, 0, 1, 1,1,1e15 )
-		values = bcdfo_evalL_( QZ, RZ, Y, np.array(range(1,6)), np.array([[-1], [1]]), xbase, 0, scale, 1 )
-		#print "values", values
-		
-
-#Ankes Test
-#Y = np.array([[ 0, 1, 0, 2, 1, 0],[ 0, 0, 1, 0, 0.01, 2 ]])
-#n,p1=np.shape(Y)
-#I=np.eye(p1)
-#lc     = len(np.array([1,2,3,4,5]))
-#q=( ( n + 1 ) * ( n + 2 ) ) / 2
-#values = np.zeros((p1,1))
-#from bcdfo_build_QR_of_Y import *
-#[QZ, RZ, xbase, scale ] = bcdfo_build_QR_of_Y( Y, 0, 1, 1, 1, 1e15 )
-#from bcdfo_evalL import *
-#x = np.array([[-1.],[1.]])
-#values = bcdfo_evalL( QZ, RZ, Y, np.array([1,2,3,4,5]), x, xbase, 0, scale, 1 )	
-
-##  should give 
-##     values =
-##         0   97.0000    2.9900    1.0000 -100.0000   -0.4950 
+    #More complicated test initally written in the matlab code
+    def test_bcdfo_evalL_5(self):
+        Y = matlabarray([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
+        QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y, 0, 1, 1,1,1e15 )
+        values = bcdfo_evalL_( QZ, RZ, Y, matlabarray(range(1,6)), matlabarray([[-1],[1]]), xbase, 0, scale, 1 )
+        #print "values", values
+        correctvalues = matlabarray([ [0],   [97.0000],    [2.9900],    [1.0000], [-100.0000],   [-0.4950] ])
+        self.assertTrue(compare_matlabarray(values, correctvalues, self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(xbase, matlabarray([0,0]), self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(scale, matlabarray([1,0.5, 0.5, 0.25, 0.25, 0.25]), self.abs_tol, self.rel_tol))
+        
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
