6e0249f5eda405f96edce932ef692fdd552974c4
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-09 10:44:33 +0200

    Amelioration of the tests
    We verify simply here that the tests give exactly the same results both in matlab and python.

--- tests/bcdfo_solve_TR_MS_test.py
+++ tests/bcdfo_solve_TR_MS_test.py
@@ -7,32 +7,51 @@ Created on Tue Dec 02 16:15:18 2014
 
 import unittest
 from bcdfo_solve_TR_MS import bcdfo_solve_TR_MS_
-from runtime import matlabarray
+from runtime import matlabarray, compare_matlabarray
 #import numpy as np
 #import helper
 
 class Test_bcdfo_solve_TR_MS(unittest.TestCase):
+    """
+      Reminder :
+      This class is a test for solve_TR_MS which as its name indicates, solves the Trust Region minimization based on the More-Sorensen algorithm
+    """ 
+    def setUp(self):
+        #self.options = helper.dummyOptions()
+        #self.values = helper.dummyValues()
+         self.abs_tol=1e-13;
+         self.rel_tol=1e-13;
+         pass
 
-	def setUp(self):
-		#self.options = helper.dummyOptions()
-		#self.values = helper.dummyValues()
-
-		pass
-
-	def test_bcdfo_solve_TR_MS(self):
-		#  TEST:
-
-		ans = bcdfo_solve_TR_MS_( matlabarray([ 2 , 3 ]), matlabarray([[ 4, 6], [6, 5 ]]), 1.0, 0.001 )
-		#print ans
-		correctans = matlabarray( [0.5153, -0.8575])
-		self.assertTrue((abs(ans[0] - correctans) < 1e-4).all())
-#  should give
-#    0.5153
-#   -0.8575
-#		bcdfo_solve_TR_MS( [ 2 ; 0 ], [ 4 0; 0 -15 ], 1.0, 0.001 )
-#  should give
-#   -0.1053
-#    0.9944
+    def test_bcdfo_solve_TR_MS(self):
+        """
+            Tests initially written in the matlab code.  We compare the results from python with the results from matlab and verify that it is correct
+        """
+        s, lamb, norms, value, gplus, nfact, neigd, msg, hardcase = bcdfo_solve_TR_MS_( matlabarray([ 2 , 3 ]), matlabarray([[ 4, 6], [6, 5 ]]), 1.0, 0.001 )
+        correctS = matlabarray( [0.515282741049029, -0.8575287994226390])
+        correctgplus=matlabarray( [-1.084041832339715, 1.804052449180985])
+        self.assertTrue(compare_matlabarray(correctS, s, self.abs_tol, self.rel_tol))
+        self.assertAlmostEqual(lamb, 2.103780596518304, places=15)
+        self.assertAlmostEqual(norms, 1.000435877536504, places=15)
+        self.assertAlmostEqual(value, -1.823817946895660, places=15)
+        self.assertTrue(compare_matlabarray(correctgplus, gplus, self.abs_tol, self.rel_tol))
+        self.assertEqual(nfact,8)
+        self.assertEqual(neigd,0)
+        self.assertEqual(str(msg), 'boundary solution')
+        self.assertEqual(hardcase,0)
+        
+        s, lamb, norms, value, gplus, nfact, neigd, msg, hardcase =bcdfo_solve_TR_MS_( matlabarray([ 2 , 0 ]), matlabarray([[ 4, 0], [0, -15 ]]), 1.0, 0.001 )
+        correctS = matlabarray( [-0.1052631578947368,0.9944444014574307])
+        correctgplus=matlabarray( [1.578947368421053, -14.91666602186146])
+        self.assertTrue(compare_matlabarray(correctS, s, self.abs_tol, self.rel_tol))
+        self.assertAlmostEqual(lamb, 15, places=15)
+        self.assertAlmostEqual(norms, 1, places=15)
+        self.assertAlmostEqual(value, -7.605263157894736, places=15)
+        self.assertTrue(compare_matlabarray(correctgplus, gplus, self.abs_tol, self.rel_tol))
+        self.assertEqual(nfact,45)
+        self.assertEqual(neigd,1)
+        self.assertEqual(str(msg), "[[ 'boundary solution ( 45 factorizations, 1 eigen decomposition, lambda = 15.0 )']]")
+        self.assertEqual(hardcase,1)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
