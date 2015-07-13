2f4860ee1beefb08fdf3feeabbb51f8691d6a9b2
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-13 15:36:09 +0200

    Addition and ameliorations of tests
    Verification that the results are the same as with matlab.
    
    There is still one little issue on replace code, at least on the test, which will be corrected soon.

--- tests/bcdfo_include_in_Y_test.py
+++ tests/bcdfo_include_in_Y_test.py
@@ -4,112 +4,88 @@ Created on Tue Dec 02 14:41:23 2014
 
 @author: jaco_da
 """
+import sys
+sys.path.append("../")
+sys.path.append("tests/")
+
 import unittest
 from bcdfo_include_in_Y import bcdfo_include_in_Y_
 from bcdfo_build_QR_of_Y import bcdfo_build_QR_of_Y_
-from runtime import matlabarray, char
-#import numpy as np
-#import helper
+from runtime import matlabarray, compare_matlabarray, char
+from bcdfo_evalZ import *
+import numpy as np
+import helper
 
 class Test_bcdfo_include_in_Y(unittest.TestCase):
+    """
+      Reminder :
+      This class is a test for bcdfo_include_in_Y which attempts to include x in the interpolation set by replacing an existing
+      interpolation point
+    """ 
+    def setUp(self):
+        #self.options = helper.dummyOptions()
+        #self.values = helper.dummyValues()
+        self.abs_tol=1e-15;
+        self.rel_tol=1e-15;
+        pass
 
-	def setUp(self):
-		#self.options = helper.dummyOptions()
-		#self.values = helper.dummyValues()
-
-		pass
-
-	def test_bcdfo_include_in_Y(self):
-#		TEST:
-		Y = matlabarray([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]])
-		whichmodel = 0
-		QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y, whichmodel, 1, 1, 1, 1e15 )
-		QZplus, RZplus, Yplus, pos, xbase, scale = bcdfo_include_in_Y_(matlabarray([-1,1]).T, QZ, RZ, Y, matlabarray(range(2,6)), 0.01, char('weighted'), xbase, whichmodel, 0, scale, 1, 1, 1, 1e15 )
-		#print QZplus, RZplus, Yplus, pos, xbase, scale
-		
-		correctQZplus = matlabarray([
+    def test_bcdfo_include_in_Y(self):
+#        TEST:
+        """
+            This is the test written in the Matlab Code. Results are the same except for a few signs due to a non-unique QR decomposition
+        """
+        Y = matlabarray([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]])
+        whichmodel = 0
+        QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y, whichmodel, 1, 1, 1, 1e15 )
+        QZplus, RZplus, Yplus, pos, xbase, scale = bcdfo_include_in_Y_(matlabarray([-1,1]).T, QZ, RZ, Y, matlabarray(range(2,6)), 0.01, char('weighted'), xbase, whichmodel, 0, scale, 1, 1, 1, 1e15 )
+        #print QZplus, RZplus, Yplus, pos, xbase, scale
+        
+        correctQZplus = matlabarray([
    [-1.0000,         0,         0,         0,         0,         0],
-   [     0,    0.9701,    0.0000 ,  -0.2425,    0.0000,   -0.0000],
-   [     0,    0.0000,   -0.9701,   -0.0000,    0.0000,   -0.2425],
-   [     0,    0.2425,   -0.0000,    0.9701,   -0.0000,    0.0000],
-   [     0,    0.0000,   -0.2425,    0.0000,   -0.0000,    0.9701],
+   [     0,    -9.701425001453321e-01,    0.0000 ,  2.425356250363330e-01,    0.0000,   -0.0000],
+   [     0,    0.0000,   -9.701425001453321e-01,   -0.0000,    0.0000,   2.425356250363330e-01],
+   [     0,    -2.425356250363330e-01,   -0.0000,    -9.701425001453319e-01,   -0.0000,    0.0000],
+   [     0,    0.0000,   -2.425356250363330e-01,    0.0000,   -0.0000,    -9.701425001453319e-01],
    [     0,   -0.0000,    0.0000,    0.0000,    1.0000,   0.0000]])
 
-		correctRZplus = matlabarray([
+        correctRZplus = matlabarray([
    [-1.0000,   -1.0000,   -1.0000,   -1.0000,   -1.0000,   -1.0000],
-   [      0,    0.5154,         0,    1.0914,   -0.4548,    0.0000],
-   [      0,         0,   -0.5154,   -0.0000,   -0.5154,   -1.0914],
-   [      0,         0,         0,    0.2425,    0.2425,   -0.0000],
+   [      0,    -5.153882032022076e-01,         0,    -1.091410312663498e+00,   4.547542969431244e-01,    0.0000],
+   [      0,         0,   -5.153882032022076e-01,   -0.0000,   -5.153882032022077e-01,   -1.091410312663498e+00],
+   [      0,         0,         0,    -2.425356250363330e-01,    -2.425356250363330e-01,   -0.0000],
    [      0,         0,         0,         0,   -0.2500,   -0.0000],
-   [      0,         0,         0,         0,         0,    0.2425]])
+   [      0,         0,         0,         0,         0,    -2.425356250363330e-01]])
 
-		correctYplus = matlabarray([
+        correctYplus = matlabarray([
 
      [0,     1,     0,     2,    -1,     0],
      [0,     0,     1,     0,     1,     2]])
 
-		correctpos = 4  #5 is the matlabindex
-		correctxbase = matlabarray([[0],[0]])
-		correctscale = matlabarray([ 
+        correctpos = 4  #5 is the matlabindex
+        correctxbase = matlabarray([[0],[0]])
+        correctscale = matlabarray([ 
     [1.0000],
     [0.5000],
     [0.5000],
     [0.2500],
     [0.2500],
     [0.2500],])
-				
-		#self.assertTrue((abs(correctQZplus - QZplus) < 1e-8).all())
-		#self.assertTrue((abs(correctRZplus - RZplus) < 1e-8).all())
-		#print "QR decompositions is different again"
-		self.assertTrue((abs(correctYplus - Yplus) < 1e-8).all())
-		self.assertEqual(correctpos, pos)
-		self.assertAlmostEqual(correctxbase, xbase)
-		self.assertAlmostEqual(correctscale, scale)
-	
-#  QZplus =
-#
-#   -1.0000         0         0         0         0         0
-#         0    0.9701    0.0000   -0.2425    0.0000   -0.0000
-#         0    0.0000   -0.9701   -0.0000    0.0000   -0.2425
-#         0    0.2425   -0.0000    0.9701   -0.0000    0.0000
-#         0    0.0000   -0.2425    0.0000   -0.0000    0.9701
-#         0   -0.0000    0.0000    0.0000    1.0000    0.0000
-#
-#  RZplus =
-#
-#   -1.0000   -1.0000   -1.0000   -1.0000   -1.0000   -1.0000
-#         0    0.5154         0    1.0914   -0.4548    0.0000
-#         0         0   -0.5154   -0.0000   -0.5154   -1.0914
-#         0         0         0    0.2425    0.2425   -0.0000
-#         0         0         0         0   -0.2500   -0.0000
-#         0         0         0         0         0    0.2425
-#
-#  Yplus =
-#
-#     0     1     0     2    -1     0
-#     0     0     1     0     1     2
-#
-#  pos =
-#
-#     5
-#
-#  xbase =
-#
-#     0
-#     0
-#
-#  scale =
-#
-#    1.0000
-#    0.5000
-#    0.5000
-#    0.2500
-#    0.2500
-#    0.2500
-#
-#  and
-#  RZplus\(QZplus'*bcdfo_evalZ((Yplus-Y(:,1)*ones(1,6))*scale(2),6))
-#  should give the identity matrix.
+                
+        #print "QR decompositions is different"
+#        print QZ
+#        print RZ
+        self.assertTrue(compare_matlabarray(correctQZplus, QZplus, self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(correctRZplus, RZplus, self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(correctYplus, Yplus, self.abs_tol, self.rel_tol))
+        self.assertEqual(correctpos, pos)
+        self.assertAlmostEqual(correctxbase, xbase)
+        self.assertAlmostEqual(correctscale, scale)
+        var=np.dot(helper.convert(QZplus.T), bcdfo_evalZ((helper.convert((Yplus)-np.dot(helper.convert(Y[:,1].T), np.ones((1,6))))*helper.convert(scale[2])),6))
+        ident=np.linalg.solve(var,helper.convert(RZplus))
+        self.assertTrue(compare_matlabarray(matlabarray(ident), matlabarray(np.eye(6)),self.abs_tol, self.rel_tol))
+
+       #  RZplus\(QZplus'*bcdfo_evalZ((Yplus-Y(:,1)*ones(1,6))*scale(2),6))
+       #  should gives the identity matrix.
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- tests/bcdfo_replace_in_Y_test.py
+++ tests/bcdfo_replace_in_Y_test.py
@@ -4,100 +4,75 @@ Created on Tue Dec 02 15:23:24 2014
 
 @author: jaco_da
 """
+import sys
+sys.path.append("../")
+sys.path.append("tests/")
 
 import unittest
 from bcdfo_replace_in_Y import *#bcdfo_replace_in_Y_
 from bcdfo_build_QR_of_Y import *#bcdfo_build_QR_of_Y_
-from runtime import matlabarray
+from runtime import matlabarray, compare_matlabarray
 import numpy as np
 #import helper
 
 class Test_bcdfo_replace_in_Y(unittest.TestCase):
+    """
+      Reminder :
+      This class is a test for bcdfo_replace_in_Y which updates the interpolation set for a transformation of Y in Yplus where the
+      vector ynew replaces Y(:,j).  Also update the factorization of Z(Y)
+      accordingly.
+    """ 
+    def setUp(self):
+        #self.options = helper.dummyOptions()
+        #self.values = helper.dummyValues()
+        self.abs_tol=1e-10;
+        self.rel_tol=1e-10
+        pass
 
-	def setUp(self):
-		#self.options = helper.dummyOptions()
-		#self.values = helper.dummyValues()
+    #@unittest.expectedFailure
+    def test_bcdfo_replace_in_Y(self):
+        """
+        This is the test written in the Matlab Code. Results are the same.
+        """
+        Y = matlabarray([[ 1.0, 1.0, 0.0, 0.0, 0.0, -1.0], [1.0, 0.0, -1.0, 1.0, 0.0, 0.0, ]])
+        whichmodel = 0
+        QZ,RZ,xbase,scale = bcdfo_build_QR_of_Y_( Y , 0, 0, 1, 1, 1e15 )
+        #print "QZ\n", QZ
+        #print "RZ\n", RZ
 
-		pass
-
-	#@unittest.expectedFailure
-	def test_bcdfo_replace_in_Y(self):
-		Y = matlabarray([[ 1.0, 1.0, 0.0, 0.0, 0.0, -1.0], [1.0, 0.0, -1.0, 1.0, 0.0, 0.0, ]])
-		whichmodel = 0
-		QZ,RZ,xbase,scale = bcdfo_build_QR_of_Y( Y , 0, 1, 1, 1, 1e15 )
-		#print "QZ\n", QZ
-		#print "RZ\n", RZ
-
-		#RZ = np.array([
-
-    #[1.0000,    1.0000,    1.0000,    1.0000,   1.0000,    1.0000],
-    # [    0.0,   -0.4583,   -0.9602,         0,  -0.4583,   -0.4583],
-    #  [   0.0,         0,    0.6388,    0.3287,    0.4540,    0.9392],
-    #  [   0,         0,         0,   -0.3193,   -0.1903,   -0.4111],
-    #  [   0,         0,         0,         0,    0.0877,    0.1754],
-    #  [   0,         0,         0,         0,         0,    0.1952		]])
-		
-		ynew = 0.25*Y[:,3].T
-		#print "ynew", ynew
-		QZplus, RZplus, Yplus, xbase, scale = bcdfo_replace_in_Y( QZ, RZ, ynew, Y, 3, xbase, whichmodel, scale, 1, 1, 1, 1e15 )
-		
-		#print "QZplus:\n", QZplus, "\nRZplus:\n",  RZplus, "\nYplus:\n", Yplus
-		
-		correctYplus = matlabarray([
+        ynew = 0.25*Y[:,3].T
+        #print "ynew", ynew
+        QZplus, RZplus, Yplus, xbase, scale = bcdfo_replace_in_Y_( QZ, RZ, ynew, Y, 3, xbase, whichmodel, scale, 0, 1, 1, 1e15 )
+        
+        #print "QZplus:\n", QZplus, "\nRZplus:\n",  RZplus, "\nYplus:\n", Yplus
+        
+        correctYplus = matlabarray([
     [1.0000,    1.0000,         0,         0,         0,   -1.0000],
     [1.0000,         0,   -0.2500,    1.0000,         0,         0]])
-				
-		self.assertTrue((abs(Yplus - correctYplus) < 1e-8).all())
-		
-				
-#		correctQZplus = matlabarray([
-#   [-0.4714,   -0.4714,    0.7205,    0.1527,    0.1149,    0.0000],
-#   [-0.4714,   -0.4714,   -0.5764,   -0.1221,   -0.0919,    0.4472],
-#   [-0.4714,    0.4714,   -0.1891,    0.6333,    0.3446,    0.0000],
-#   [-0.2357,   -0.2357,   -0.2882,   -0.0611,   -0.0459,   -0.8944],
-#   [-0.2357,    0.2357,    0.1081,    0.1813,   -0.9189,   -0.0000],
-#   [-0.4714,    0.4714,    0.1351,   -0.7240,    0.1149,   -0.0000]])
+                  
+        correctQZplus = matlabarray([
+   [-4.714045207910318e-01,   -4.714045207910318e-01,                    0.720457635944399,    0.152655619705795	, 0.114859096884849	,3.42976275268113e-17],
+   [-4.714045207910317e-01,   -4.714045207910317e-01,   -0.576366108755519,   -0.122124495764636,	-0.0918872775078794	,-0.447213595499958],
+   [-4.714045207910317e-01,   4.714045207910317e-01,   -0.189120129435405,    0.633289525385556,	0.344577290654547,	1.09881792804699e-17],
+   [-2.357022603955159e-01,   -2.357022603955158e-01,   -0.288183054377760,   -0.0610622478823181,	-0.0459436387539394,	0.894427190999916],
+   [-2.357022603955159e-01,    2.357022603955158e-01,    0.108068645391660,   0.181336372499005,	-0.918872775078793,	-2.55294270180895e-16],
+   [-4.714045207910317e-01,    4.714045207910317e-01,    0.135085806739575,   -0.723957711635059,	0.114859096884849	,-1.00034123182046e-16]])
 
-#		correctRZplus = matlabarray([
-#   [-2.1213,   -1.0607,   -0.3609,   -1.0607 ,  -0.4714,   -0.1179],
-#   [      0,   -1.0607,   -0.5819,    0.1179,   -0.4714,   -0.1179],
-#   [      0,         0,  0.7711 ,   0.5854 ,   0.7205 ,   1.1527],
-#   [      0,         0,        0,    0.8766 ,   0.1527,    0.2442],
-#   [      0,         0,         0,         0,    0.1149,    0.1838],
-#   [      0,        0,         0 ,        0,         0 ,  -0.8944]])
+        correctRZplus=matlabarray([
+[-2.12132034355964,	-1.06066017177982,	-0.360919086230634,	-1.06066017177982	,-0.471404520791032,	-0.117851130197758],
+[0,	-1.06066017177982,	-0.581889955351430,	0.117851130197758,	-0.471404520791032,	-0.117851130197758],
+[0,	0,	0.771114813471740,	0.585371829204824,	0.720457635944399	,1.15273221751104],
+[0,	0,	0,	0.876613331340854,	0.152655619705795,	0.244248991529272],
+[0,	0,	0,	0,	0.114859096884849,	0.183774555015759],
+[0,	0,	0,	0,	0,	0.894427190999916]])
+         
+#        print QZplus[6,:]
+#        print correctQZplus
+        self.assertTrue(compare_matlabarray(correctQZplus, QZplus,self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(correctRZplus, RZplus,self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(correctYplus, Yplus,self.abs_tol, self.rel_tol))
 
-#		correctYplus = matlabarray([
-#    [1.0000,    1.0000,         0,         0,         0,   -1.0000],
-#    [1.0000,         0,   -0.2500,    1.0000,         0,         0]])
-		
-		
-	def est_bcdfo_replace_in_YOriginal(self):
-		Y = np.array([[ 1.0, 1.0, 0.0, 0.0, 0.0, -1.0], [1.0, 0.0, -1.0, 1.0, 0.0, 0.0, ]])
-		whichmodel = 0
-		QZ,RZ,xbase,scale = bcdfo_build_QR_of_Y( Y , 0, 1, 1, 1, 1e15 )
-		#print "QZ\n", QZ
-		#print "RZ\n", RZ
-
-		#RZ = np.array([
-
-    #[1.0000,    1.0000,    1.0000,    1.0000,   1.0000,    1.0000],
-    # [    0.0,   -0.4583,   -0.9602,         0,  -0.4583,   -0.4583],
-    #  [   0.0,         0,    0.6388,    0.3287,    0.4540,    0.9392],
-    #  [   0,         0,         0,   -0.3193,   -0.1903,   -0.4111],
-    #  [   0,         0,         0,         0,    0.0877,    0.1754],
-    #  [   0,         0,         0,         0,         0,    0.1952		]])
-		
-		ynew = np.array([0.25*Y[:,3-1]]).T
-		#print "ynew", ynew
-		QZplus, RZplus, Yplus, xbase, scale = bcdfo_replace_in_Y( QZ, RZ, ynew, Y, 3-1, xbase, whichmodel, scale, 1, 1, 1, 1e15 )
-		
-		#print "QZplus:\n", QZplus, "\nRZplus:\n",  RZplus, "\nYplus:\n", Yplus
-		
-		correctYplus = np.array([
-    [1.0000,    1.0000,         0,         0,         0,   -1.0000],
-    [1.0000,         0,   -0.2500,    1.0000,         0,         0]])
-				
-		self.assertTrue((abs(Yplus - correctYplus) < 1e-8).all())
+        
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
