bdc9e9fa33887782600c59e60b5e2b95c8d302d1
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-14 10:42:27 +0200

    Addition of a new test
    This is the matlab test with scaled version. No bug found.

--- tests/bcdfo_replace_in_Y_test.py
+++ tests/bcdfo_replace_in_Y_test.py
@@ -25,14 +25,14 @@ class Test_bcdfo_replace_in_Y(unittest.TestCase):
     def setUp(self):
         #self.options = helper.dummyOptions()
         #self.values = helper.dummyValues()
-        self.abs_tol=1e-14;
-        self.rel_tol=1e-14;
+        self.abs_tol=1e-10;
+        self.rel_tol=1e-10;
         pass
 
     #@unittest.expectedFailure
     def test_bcdfo_replace_in_Y(self):
         """
-        This is the test written in the Matlab Code. Results are the same.
+        This is the test written in the Matlab Code. Results are the same except for maybe some signs which may be different due to a slightly different QR factorization.
         """
         Y = matlabarray([[ 1.0, 1.0, 0.0, 0.0, 0.0, -1.0], [1.0, 0.0, -1.0, 1.0, 0.0, 0.0, ]])
         whichmodel = 0
@@ -66,12 +66,43 @@ class Test_bcdfo_replace_in_Y(unittest.TestCase):
 [0,	0,	0,	0,	0.114859096884849,	0.183774555015759],
 [0,	0,	0,	0,	0,	0.894427190999916]])
          
-#        print correctQZplus
+#        print QZplus
         self.assertTrue(compare_matlabarray(correctQZplus, QZplus,self.abs_tol, self.rel_tol)) 
         self.assertTrue(compare_matlabarray(correctRZplus, RZplus,self.abs_tol, self.rel_tol))
         self.assertTrue(compare_matlabarray(correctYplus, Yplus,self.abs_tol, self.rel_tol))
 
+        #the same test with scale version :
+        Y = matlabarray([[ 1.0, 1.0, 0.0, 0.0, 0.0, -1.0], [1.0, 0.0, -1.0, 1.0, 0.0, 0.0, ]])
+        whichmodel = 0
+        QZ,RZ,xbase,scale = bcdfo_build_QR_of_Y_( Y , 0, 1, 1, 1, 1e15 )
+
+        ynew = 0.25*Y[:,3].T
         
+        QZplus, RZplus, Yplus, xbase, scale = bcdfo_replace_in_Y_( QZ, RZ, ynew, Y, 3, xbase, whichmodel, scale, 1, 1, 1, 1e15 )
+        
+        correctYplus = matlabarray([
+    [1.0000,    1.0000,         0,         0,         0,   -1.0000],
+    [1.0000,         0,   -0.2500,    1.0000,         0,         0]])
+    
+        correctQZplus=matlabarray([
+    [-1,	0,	0,	0,	0,	0],
+[0,	0,	-0.855247610220823,	0.470034549360494,	3.88578058618805e-16,	-0.218217890235992],
+[0,	0.975900072948533	, 0.0127268989616193,	0.0231571273585230,	-0.216612144429553,	-4.44089209850063e-16],
+[0,	0,	0.191239179404800,	-0.105102920414355,	1.91166527052644e-15,	-0.975900072948533],
+[0,	-0.218217890235992,	0.0569164224419050,	0.103561821874555	  ,-0.968718959392966,	-1.85962356624714e-15],
+[0,	0,	0.478097948512001, 	0.869919303746259	,0.121089869924121,	6.03683769639929e-16]])
+        
+        correctRZplus=matlabarray([
+[-1,	-1,	-1,	-1,	-1,	-1],
+[0,	-0.458257569495584,	-0.579641270939355,	0,	-0.458257569495584,	-0.458257569495584],
+[0,	0,	0.522905402079391	, 0.401602276750081,	0.497221866452481	, 1.03269156878592],
+[0,	0,	0,	-0.220716132870144,	-0.0467322721208927,	-0.114485128324656],
+[0,	0,	0,	0,	0.0242179739848241,	0.0484359479696486],
+[0,	0,	0,	0,	0,	-0.195180014589707]])
+
+        self.assertTrue(compare_matlabarray(correctQZplus, QZplus,self.abs_tol, self.rel_tol)) 
+        self.assertTrue(compare_matlabarray(correctRZplus, RZplus,self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_matlabarray(correctYplus, Yplus,self.abs_tol, self.rel_tol))
 
 if __name__ == '__main__':
     unittest.main()
