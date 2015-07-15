007de35b14421de73890b88c4036dbf14fc7a87b
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-15 16:25:17 +0200

    Commented and modified slightly the tests

--- tests/sqplab_badsimul_test.py
+++ tests/sqplab_badsimul_test.py
@@ -4,6 +4,8 @@ Created on Fri Nov 07 15:17:32 2014
 
 @author: jaco_da
 """
+import sys
+sys.path.append("../")
 
 import unittest
 from sqplab_badsimul import *
@@ -11,30 +13,37 @@ from sqplab_badsimul import *
 import helper
 
 class dummyInfo():
-	def __init__(self):
-		self.flag = False
+    def __init__(self):
+        self.flag = False
 
 class Test_sqplab_badsimul(unittest.TestCase):
+    """
+          Reminder :
+          This class is a test for sqplab_badsimul
+    """
+    def setUp(self):
+        self.dummyOptions = helper.dummyOptions()
+        self.dummyInfo = dummyInfo()
+        self.dummyValues = helper.dummyValues()
 
-	def setUp(self):
-		self.dummyOptions = helper.dummyOptions()
-		self.dummyInfo = dummyInfo()
-		self.dummyValues = helper.dummyValues()
-
-	def test_sqplab_bad_simul1(self):
-		self.dummyValues.stop_on_simul = True
-		self.dummyInfo = sqplab_badsimul_(outdic=2,info=self.dummyInfo,options=self.dummyOptions,values=self.dummyValues)
-		
-		#print self.dummyInfo.flag
-		self.assertTrue(self.dummyInfo.flag)
-	
-	def test_sqplab_bad_simul2(self):
-		self.dummyValues.fail_on_simul = True
-		self.dummyInfo = sqplab_badsimul_(outdic=3,info=self.dummyInfo,options=self.dummyOptions,values=self.dummyValues)
-		
-		#print self.dummyInfo.flag
-		self.assertTrue(self.dummyInfo.flag)
+    def test_sqplab_bad_simul1(self):
+        """
+               Test when oudic=2
+        """
+        self.dummyInfo = sqplab_badsimul_(outdic=2,info=self.dummyInfo,options=self.dummyOptions,values=self.dummyValues)
+        
+#        print self.dummyInfo.flag
+        self.assertTrue(self.dummyInfo.flag==self.dummyValues.stop_on_simul)
+    
+    def test_sqplab_bad_simul2(self):
+        """
+               Test when oudic>2
+        """
+        self.dummyInfo = sqplab_badsimul_(outdic=3,info=self.dummyInfo,options=self.dummyOptions,values=self.dummyValues)
+        
+        #print self.dummyInfo.flag
+        self.assertTrue(self.dummyInfo.flag==self.dummyValues.fail_on_simul)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
