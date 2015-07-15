7775f7a62e82ee29bac1c69ae49b0ad69e4daae5
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-15 14:48:11 +0200

    Amelioration of the comments and the test

--- tests/blls_test.py
+++ tests/blls_test.py
@@ -4,36 +4,44 @@ Created on Fri Nov 07 11:52:38 2014
 
 @author: jaco_da
 """
+import sys
+sys.path.append("../")
 
 import unittest
 from blls import *
-import numpy as np
 #import helper
 
 class Test_blls(unittest.TestCase):
+    """
+      Reminder :
+      This class is a test for blls which is a solver for bound-counstrained linear least-squares problems.
+    """ 
+    def setUp(self):
+        self.abs_tol=1e-15;
+        self.rel_tol=1e-15;
 
-	def setUp(self):
-		self.A = matlabarray([[6,     0,     4],
-				[1,     5,     7],
-			     [0,     2,     2],
-			     [3,     7,     0]])
+    def test_blls_1(self):
+        """
+              test with  some random data. Results have been compared with matlab results : it is OK
+        """
+        self.A = matlabarray([[6,     0,     4],
+                [1,     5,     7],
+                 [0,     2,     2],
+                 [3,     7,     0]])
 
-		self.b = matlabarray([1, 5, 4, 3]).T
+        self.b = matlabarray([1, 5, 4, 3]).T
 
-#>> blls(A,b, lb, ub)
+        self.lb = matlabarray([-1, -1, -1]).T
+        self.ub = -self.lb
+        
+        s, resn, opt, exitc = blls_(self.A,self.b, self.lb, self.ub)
 
-		self.ans =matlabarray([-0.1713, 0.5162, 0.4663]).T
-
-		self.lb = matlabarray([-1, -1, -1]).T
-		self.ub = -self.lb
-
-	def test_blls(self):
-		res = blls_(self.A,self.b, self.lb, self.ub)
-		
-		#print "res", res[0]
-		#print "ans", self.ans
-		np.testing.assert_almost_equal(res[0], self.ans, 4)
-		
+        correctS =matlabarray([ -0.171259842519685, 0.516235951275322, 0.466316710411199]).T
+        self.assertTrue(compare_matlabarray(correctS, s, self.abs_tol, self.rel_tol))
+        self.assertAlmostEqual(resn,2.152108290072786, places=10)
+        self.assertAlmostEqual(opt,3.631770138003687e-14, places=10)
+        self.assertEqual(exitc, 0)
+         
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
