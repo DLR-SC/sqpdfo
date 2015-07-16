4b5f0eb274f8c4261892f295f6555ba18e34733a
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-16 08:47:53 +0200

    Addition of a few comments

--- tests/ecdfo_func_test.py
+++ tests/ecdfo_func_test.py
@@ -32,7 +32,8 @@ ce =
    0.001195859492999
 @author: jaco_da
 """
-
+import sys
+sys.path.append("../")
 import unittest
 from ecdfo_func import *
 #import numpy as np
@@ -40,32 +41,39 @@ from ecdfo_func import *
 
 
 class Test_ecdfo_func(unittest.TestCase):
+    """
+          Reminder :
+          This class is a test for ecdfo_func which computes f, ci and ce according to a given problem
+    """
+    def setUp(self):
+        self.x =  matlabarray([ -1.717000000000000,   1.595700000000000,   1.827200000000000,  -0.763600000000000,  -0.763600000000000])
+        #self.options = helper.dummyOptions()
+        #self.values = helper.dummyValues()
+        self.abs_tol=1e-11
+        self.rel_tol=1e-11
+        pass
 
-	def setUp(self):
-		self.x =  matlabarray([ -1.717000000000000,   1.595700000000000,   1.827200000000000,  -0.763600000000000,  -0.763600000000000])
-		#self.options = helper.dummyOptions()
-		#self.values = helper.dummyValues()
-
-		pass
-
-	
-	def test_ecdfo_func(self):
-		set_prob(5)
-		
-		msg,f,ci,ce = ecdfo_func_(self.x)
-		
-		correctmsg = 0
-		correctf =0.053985698962084
-		correctci = matlabarray([])
-		correctce = matlabarray([  -0.000822750000001, 0.000238240000000, 0.001195859492999]).T
-		
-		self.assertEqual(correctmsg, msg)
-		self.assertAlmostEqual(correctf, f, 7)
-		self.assertAlmostEqual(correctci, ci, 7)
-		#print "abs", (abs(correctce - ce) < 1e-7).all()
-		#print "ce", ce
-		#print "correctce", correctce
-		self.assertTrue((abs(correctce - ce) < 1e-7).all())
+    
+    def test_ecdfo_func(self):
+        """
+          Test with problem 5, results compared with matlab
+        """
+        set_prob(5)
+        
+        msg,f,ci,ce = ecdfo_func_(self.x)
+        
+        correctmsg = 0
+        correctf =0.053985698962084
+        correctci = matlabarray([])
+        correctce = matlabarray([  -0.000822750000001, 0.000238240000000, 0.001195859492999]).T
+        
+        self.assertEqual(correctmsg, msg)
+        self.assertAlmostEqual(correctf, f, 7)
+        self.assertAlmostEqual(correctci, ci, 7)
+        #print "abs", (abs(correctce - ce) < 1e-7).all()
+        #print "ce", ce
+        #print "correctce", correctce
+        self.assertTrue(compare_matlabarray(correctce, ce, self.abs_tol, self.rel_tol))
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
