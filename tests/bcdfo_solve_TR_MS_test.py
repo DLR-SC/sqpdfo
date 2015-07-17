6558684afad4d76ddec3efaf27355bf3264be43a
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-17 09:21:13 +0200

    Small modification on the string msg

--- tests/bcdfo_solve_TR_MS_test.py
+++ tests/bcdfo_solve_TR_MS_test.py
@@ -30,18 +30,18 @@ class Test_bcdfo_solve_TR_MS(unittest.TestCase):
         """
             Tests initially written in the matlab code.  We compare the results from python with the results from matlab and verify that it is correct
         """
-#        s, lamb, norms, value, gplus, nfact, neigd, msg, hardcase = bcdfo_solve_TR_MS_( matlabarray([[ 2] ,[ 3] ]), matlabarray([[ 4, 6], [6, 5 ]]), 1.0, 0.001 )
-#        correctS = matlabarray( [0.515282741049029, -0.8575287994226390])
-#        correctgplus=matlabarray( [-1.084041832339715, 1.804052449180985])
-#        self.assertTrue(compare_matlabarray(correctS, s, self.abs_tol, self.rel_tol))
-#        self.assertAlmostEqual(lamb, 2.103780596518304, places=15)
-#        self.assertAlmostEqual(norms, 1.000435877536504, places=15)
-#        self.assertAlmostEqual(value, -1.823817946895660, places=15)
-#        self.assertTrue(compare_matlabarray(correctgplus, gplus, self.abs_tol, self.rel_tol))
-#        self.assertEqual(nfact,8)
-#        self.assertEqual(neigd,0)
-#        self.assertEqual(str(msg), 'boundary solution')
-#        self.assertEqual(hardcase,0)
+        s, lamb, norms, value, gplus, nfact, neigd, msg, hardcase = bcdfo_solve_TR_MS_( matlabarray([[ 2] ,[ 3] ]), matlabarray([[ 4, 6], [6, 5 ]]), 1.0, 0.001 )
+        correctS = matlabarray( [0.515282741049029, -0.8575287994226390])
+        correctgplus=matlabarray( [-1.084041832339715, 1.804052449180985])
+        self.assertTrue(compare_matlabarray(correctS, s, self.abs_tol, self.rel_tol))
+        self.assertAlmostEqual(lamb, 2.103780596518304, places=15)
+        self.assertAlmostEqual(norms, 1.000435877536504, places=15)
+        self.assertAlmostEqual(value, -1.823817946895660, places=15)
+        self.assertTrue(compare_matlabarray(correctgplus, gplus, self.abs_tol, self.rel_tol))
+        self.assertEqual(nfact,8)
+        self.assertEqual(neigd,0)
+        self.assertEqual(str(msg), 'boundary solution')
+        self.assertEqual(hardcase,0)
         
         s, lamb, norms, value, gplus, nfact, neigd, msg, hardcase =bcdfo_solve_TR_MS_( matlabarray([ [2] , [0] ]), matlabarray([[ 4, 0], [0, -15 ]]), 1.0, 0.001 )
         correctS = matlabarray( [-0.1052631578947368,0.9944444014574307])
@@ -53,8 +53,8 @@ class Test_bcdfo_solve_TR_MS(unittest.TestCase):
         self.assertTrue(compare_matlabarray(correctgplus, gplus, self.abs_tol, self.rel_tol))
         self.assertEqual(nfact,45)
         self.assertEqual(neigd,1)
-        print msg
-#        self.assertEqual(str(msg), "[[ 'boundary solution ( 45 factorizations, 1 eigen decomposition, lambda = 15.0 )']]")
+        self.assertEqual(str(msg), "['boundary solution ( 45 factorizations, 1 eigen decomposition, lambda = 15.0 )']")
+#        self.assertEqual(str(msg), "[[ 'boundary solution ( 45 factorizations, 1 eigen decomposition, lambda = 15.0 )']]") # for Anke's translation
         self.assertEqual(hardcase,1)
 
 if __name__ == '__main__':
