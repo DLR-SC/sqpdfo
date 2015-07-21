a60818488b3be3c6467751228ddfd92df03eda95
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-21 16:28:02 +0200

    Replacement of Anke's code by automatically translated code

--- bcdfo_poisedness_Y.py
+++ bcdfo_poisedness_Y.py
@@ -1,106 +1,29 @@
-#!/usr/local/bin/python
-from numpy import *
-from bcdfo_find_new_yj import *
-import helper
-from runtime import matlabarray
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 16:25:40 2015
 
-@helper.convertingDecorator
-def bcdfo_poisedness_Y_( QZ, RZ, Y, eps_L, xbase, lSolver, whichmodel, hardcons, xl, xu, indfree, stratLam, scale, shift_Y, nargout=None ):
-#	QZ = helper.convert(QZ)
-#	RZ = helper.convert(RZ)
-#	Y = helper.convert(Y)
-#	eps_L = helper.convert(eps_L)
-#	xbase = helper.convert(xbase)
-#	lSolver = helper.convert(lSolver)
-#	whichmodel = helper.convert(whichmodel)
-#	hardcons = helper.convert(hardcons)
-#	xl = helper.convert(xl)
-#	xu = helper.convert(xu)
-#	indfree = helper.convert(indfree - 1)
+@author: lien_ol
+"""
 
-	#print "indfree before = ", indfree	
-	#print "len indfree", len(indfree)
-	#print "ones", ones(len(indfree))
+from __future__ import division
+try:
+    from runtime import *
+except ImportError:
+    from smop.runtime import *
+    
+def bcdfo_poisedness_Y_(QZ=None,RZ=None,Y=None,eps_L=None,xbase=None,lSolver=None,whichmodel=None,hardcons=None,xl=None,xu=None,indfree=None,stratLam=None,scale=None,shift_Y=None,*args,**kwargs):
+#    varargin = cellarray(args)
+#    nargin = 14-[QZ,RZ,Y,eps_L,xbase,lSolver,whichmodel,hardcons,xl,xu,indfree,stratLam,scale,shift_Y].count(None)+len(args)
 
-	#print "indfree after = ", indfree
-	
-#	stratLam = helper.convert(stratLam)
-#	scale = helper.convert(scale)
-#	shift_Y = helper.convert(shift_Y)
-	
-	
-#	lambd, Y_radius = bcdfo_poisedness_Y( QZ, RZ, Y, eps_L, xbase, lSolver, whichmodel, hardcons, xl, xu, indfree, stratLam, scale, shift_Y )
-	
-#	lambd = matlabarray(lambd)
-#	Y_radius = matlabarray(Y_radius)
- 
-	
-#	return lambd, Y_radius
-	
-	return bcdfo_poisedness_Y( QZ, RZ, Y, eps_L, xbase, lSolver, whichmodel, hardcons, xl, xu, indfree, stratLam, scale, shift_Y )
-
-def bcdfo_poisedness_Y( QZ, RZ, Y, eps_L, xbase, lSolver, whichmodel, hardcons, xl, xu, indfree, stratLam, scale, shift_Y ):
-   """
-   Computes the poisedness of the interpolation set Y in a ball of radius
-   Delta centered at Y(:,1), assuming that a QR factorization of the
-   interpolation system is known (and given by QZ, RZ).
-   Poisedness is defined here as the maximum aboslute value of
-   the Lagrange polynomials taken over the ball and for all polynomials.
-
-   INPUT:
-
-   QZ          : the Q matrix of the QR decomposition of Z(Y)
-   RZ          : the R matrix of the QR decomposition of Z(Y)
-   Y           : a matrix whose columns contain the current interpolation points
-   eps_L       : the relative accuracy on the trust-region constraint for
-                maximization of the Lagrange polynomials
-   xbase       : the current base point
-   lSolver     : linear solver used for the minimization of the model
-   whichmodel  : kind of model/Lagrange polynomial to compute
-   scale       : the current interpolation set scaling
-   shift_Y     : 0 if no shift in interpolation points, 1 otherwise
-
-   OUTPUT:
-
-   lambda      : the poisedness of the interpolation set Y
-   Y_radius    : the poisedness radius for Y, that is the largest distance
-                from Y(:,i) to Y(:,1) (the base point)
-
-   PROGRAMMING: Ph. Toint and S. Gratton, April 2009. (This version 22 VI 2009)
-
-   DEPENDENCIES: bcdfo_find_new_yj, bcdfo_find_new_yj_bc
-
-   CONDITIONS OF USE: Use at your own risk! No guarantee of any kind given.
-   """
-
-   lambd = 0
-   [n, p1] = shape( Y )
-   
-   #  Compute the radius of the poisedness ball.
-
-   Y_radius = 0;
-   lst = range(1,p1)
-   for j in lst:
-      Y_radius = max( Y_radius, linalg.norm( Y[:,j] - Y[:,0] ) )
-
-   #  Loop on all the possible replacements to find the best one.
-
-   for j in lst:
-      if ( hardcons == 1 ):
-   
-         [ y, improvement ] = bcdfo_find_new_yj_bc( QZ, RZ, Y, j, Y_radius, 
-          eps_L, xbase, lSolver, whichmodel, xl, xu, indfree, 
-          stratLam, scale, shift_Y );
-               
-      else:
-
-         [ y, improvement ] = bcdfo_find_new_yj( QZ, RZ, Y, j, Y_radius, eps_L, 
-         xbase, lSolver, whichmodel, scale, shift_Y );
-        
-
-      #  Remember the current polynomial value, index and replacement point if
-      #  this is the best so far.
-   
-      lambd = max( improvement, lambd );
-
-   return lambd, Y_radius
+    _lambda=0
+    n,p1=size_(Y,nargout=2)
+    Y_radius=0
+    for j in arange_(2,p1).reshape(-1):
+        Y_radius=max_(Y_radius,norm_(Y[:,j] - Y[:,1]))
+    for j in arange_(2,p1).reshape(-1):
+        if (hardcons == 1):
+            y,improvement=bcdfo_find_new_yj_bc_(QZ,RZ,Y,j,Y_radius,eps_L,xbase,lSolver,whichmodel,xl,xu,indfree,stratLam,scale,shift_Y,nargout=2)
+        else:
+            y,improvement=bcdfo_find_new_yj_(QZ,RZ,Y,j,Y_radius,eps_L,xbase,lSolver,whichmodel,scale,shift_Y,nargout=2)
+        _lambda=max_(improvement,_lambda)
+    return _lambda,Y_radius
--- bcdfo_projgrad.py
+++ bcdfo_projgrad.py
@@ -1,53 +1,26 @@
-#!/usr/local/bin/python
-from numpy import *
-import helper
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 16:25:42 2015
 
-@helper.convertingDecorator
-def bcdfo_projgrad_(n,x,g,bl,bu):
-	return bcdfo_projgrad(n,x,g,bl,bu)
+@author: lien_ol
+"""
 
+from __future__ import division
+try:
+    from runtime import *
+except ImportError:
+    from smop.runtime import *
+    
+def bcdfo_projgrad_(n=None,x=None,g=None,bl=None,bu=None,*args,**kwargs):
+#    varargin = cellarray(args)
+#    nargin = 5-[n,x,g,bl,bu].count(None)+len(args)
 
-def bcdfo_projgrad(n,x,g,bl,bu):
-########################################################
-#  This function computes the projected gradient and its
-#  infinity norm.
-#
-#  INPUTS:
-#
-#  n     : dimension
-#  x     : current iterate
-#  g     : gradient at x
-#  bl    : lower bounds
-#  bu    : upper bounds
-#
-#  OUTPUTS:
-#
-#  gnorm : infinity norm of the projected gradient
-#  gn    : projected gradient vector
-#
-########################################################
-
-   gnorm = 0.0
-   gn = array([[]])
-
-   for i in range(0,n):
-      #print "g[0]\n", g[0]				
-      gi = g[i][0]#g[0][i]
-      if ( gi < 0.0 ):
-         gn = append(gn,-min(abs(bu[i]-x[i]),-gi))
-      else:
-
-         #print "x", x
-         #print "bl", bl
-         #print "bl[i]", bl[i]									
-         #print "x[i]", x[i]									
-         #print "abs(bl[i]-x[i])", abs(bl[i]-x[i])
-         #print "gi", gi
-         #print "gn", gn									
-         #print "min(abs(bl[i]-x[i])", append(gn,min(abs(bl[i]-x[i]),gi))									
-         gn = append(gn,min(abs(bl[i]-x[i]),gi))
-      
-      # infinity-norm
-      gnorm = max(gnorm,abs(gn[i]))
-
-   return gnorm, gn
+    gnorm=0.0
+    for i in arange_(1,n).reshape(-1):
+        gi=g[i]
+        if gi < 0.0:
+            gn[i]=- min_(abs_(bu[i] - x[i]),- gi)
+        else:
+            gn[i]=min_(abs_(bl[i] - x[i]),gi)
+        gnorm=max_(gnorm,abs_(gn[i]))
+    return gnorm,gn
