ca9027eea64e9a7722c50d39797819055aa12e9d
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-21 15:26:51 +0200

    Minors modification on the automatically translated code

--- bcdfo_find_new_yj.py
+++ bcdfo_find_new_yj.py
@@ -10,11 +10,17 @@ try:
     from runtime import *
 except ImportError:
     from smop.runtime import *
+from bcdfo_computeLj import bcdfo_computeLj_
+from bcdfo_gradP import bcdfo_gradP_
+from bcdfo_hessP import bcdfo_hessP_
+from bcdfo_solve_TR_MS import bcdfo_solve_TR_MS_
+
+
 
 
 def bcdfo_find_new_yj_(QZ=None,RZ=None,Y=None,j=None,Delta=None,eps_L=None,xbase=None,lSolver=None,whichmodel=None,scale=None,shift_Y=None,*args,**kwargs):
-    varargin = cellarray(args)
-    nargin = 11-[QZ,RZ,Y,j,Delta,eps_L,xbase,lSolver,whichmodel,scale,shift_Y].count(None)+len(args)
+#    varargin = cellarray(args)
+#    nargin = 11-[QZ,RZ,Y,j,Delta,eps_L,xbase,lSolver,whichmodel,scale,shift_Y].count(None)+len(args)
 
     verbose=0
     n=size_(Y,1)
@@ -25,7 +31,7 @@ def bcdfo_find_new_yj_(QZ=None,RZ=None,Y=None,j=None,Delta=None,eps_L=None,xbase
     if (j < 2):
         return ynew,improvement,msgTR
     Lj=bcdfo_computeLj_(QZ,RZ,j,Y,whichmodel,scale,shift_Y)
-    if (length_(find_(isnan_(Lj))) != 0 or length_(find_(not isreal_(Lj))) != 0 or length_(find_(isinf_(Lj))) != 0):
+    if (length_(find_(isnan_(Lj))) != 0 or length_(find_(~ isreal_(Lj))) != 0 or length_(find_(isinf_(Lj))) != 0):
         msgTR=char('Error0: Lagrange polynomial contains NaN or Inf or nonreal components!!')
         if (verbose):
             disp_(msgTR)
@@ -35,15 +41,15 @@ def bcdfo_find_new_yj_(QZ=None,RZ=None,Y=None,j=None,Delta=None,eps_L=None,xbase
     if (shift_Y):
         g=bcdfo_gradP_(Lj,zeros_(n,1),xbase,scale,0)
         H=bcdfo_hessP_(Lj,zeros_(n,1),xbase,scale,0)
-        pstep,_lambda,norms,pvalue,gplus,nfact,neigd,msgTR=bcdfo_solve_TR_MS_(g,H,Delta * scale[2],eps_L,nargout=8)
+        pstep,_lambda,norms,pvalue,gplus,nfact,neigd,msgTR,hardcase=bcdfo_solve_TR_MS_(g,H,Delta * scale[2],eps_L,nargout=9)
         pstep=pstep / scale[2]
-        mstep,_lambda,norms,mvalue,gplus,nfact,neigd,msgTR=bcdfo_solve_TR_MS_(- g,- H,Delta * scale[2],eps_L,nargout=8)
+        mstep,_lambda,norms,mvalue,gplus,nfact,neigd,msgTR,hardcase=bcdfo_solve_TR_MS_(- g,- H,Delta * scale[2],eps_L,nargout=9)
         mstep=mstep / scale[2]
     else:
         g=bcdfo_gradP_(Lj,Y[:,1],xbase,scale,0)
         H=bcdfo_hessP_(Lj,Y[:,1],xbase,scale,0)
-        pstep,_lambda,norms,pvalue,gplus,nfact,neigd,msgTR=bcdfo_solve_TR_MS_(g,H,Delta,eps_L,nargout=8)
-        mstep,_lambda,norms,mvalue,gplus,nfact,neigd,msgTR=bcdfo_solve_TR_MS_(- g,- H,Delta,eps_L,nargout=8)
+        pstep,_lambda,norms,pvalue,gplus,nfact,neigd,msgTR,hardcase=bcdfo_solve_TR_MS_(g,H,Delta,eps_L,nargout=9)
+        mstep,_lambda,norms,mvalue,gplus,nfact,neigd,msgTR,hardcase=bcdfo_solve_TR_MS_(- g,- H,Delta,eps_L,nargout=9)
     if (verbose):
         disp_([char(' === find_new_yj: j = '),int2str_(j),char(' positive value = '),num2str_(pvalue),char(' step:')])
         pstep.T
