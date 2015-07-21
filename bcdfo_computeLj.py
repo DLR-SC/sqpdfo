d4aca6cde9ff24209d46c4eebf61a81ea5ce975f
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-21 09:21:27 +0200

    Replacement of Anke's files by automatically translated code

--- bcdfo_computeLj.py
+++ bcdfo_computeLj.py
@@ -1,158 +1,53 @@
-#!/usr/local/bin/python
-from numpy import *
-import helper
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 09:14:30 2015
 
-@helper.convertingDecorator
-def bcdfo_computeLj_( QZ, RZ, j, Y, whichmodel, scale, shift_Y ):
-	#j is an index, so we have to decrease it.
-	return bcdfo_computeLj( QZ, RZ, j-1, Y, whichmodel, scale, shift_Y )
+@author: lien_ol
+"""
 
-def bcdfo_computeLj( QZ, RZ, j, Y, whichmodel, scale, shift_Y ):
-###############################################################################
-#
-#  Computes the coefficients of the j-th Lagrange polynomial.
-#
-#  INPUTS:
-#
-#  QZ, RZ      : the QR factors of the (possibly shifted) matrix Z containing
-#                the polynomial expansion of the interpolation points
-#  j           : the index of the interpolation point one wishes to compute
-#                the associated Lagrange polynomial
-#  Y           : current interpolation set Y
-#  whichmodel  : the method to compute the polynomial
-#  scale       : scaling factor of the interpolation matrix
-#  shift_Y     : 0 if no shift in interpolation points, 1 otherwise
-#
-#  OUTPUT:
-#
-#  Lj          : a row vector containing the coefficients of the polynomial
-#
-#  DEPENDENCIES: -
-#
-#  PROGRAMMING: A. Troeltzsch, September 2010.
-#
-#  TEST:
-#  Y = [ 0 1 0 2 0 ; 0 0 1 0 2 ];  whichmodel = 0;
-#  [ QZ, RZ, xbase, scale ] = bcdfo_build_QR_of_Y(  Y, whichmodel, 1 );
-#  Lj = bcdfo_computeLj( QZ, RZ, 1, Y, whichmodel, scale, 1 )
-#  should give:
-#  Lj =
-#   1.0000  -3.0000  -3.0000   4.0000   4.0000
-#
-#  CONDITIONS OF USE: Use at your own risk! No guarantee of any kind given.
-#
-###############################################################################
+from __future__ import division
+try:
+    from runtime import *
+except ImportError:
+    from smop.runtime import *
+from bcdfo_evalZ import *
 
-    n, p1 = shape( Y )
-    q = ( ( n + 1 ) * ( n + 2 ) ) / 2
-    
-    vzeros = zeros((j,1))
-    vzeros = append(vzeros,1)
-    vzeros = append(vzeros, zeros((p1-j-1,1)))
+def bcdfo_computeLj_(QZ=None,RZ=None,j=None,Y=None,whichmodel=None,scale=None,shift_Y=None,*args,**kwargs):
+#    varargin = cellarray(args)
+#    nargin = 7-[QZ,RZ,j,Y,whichmodel,scale,shift_Y].count(None)+len(args)
 
-    #  as long as regression model is underdetermined: use min l2-norm model
-
-    if ( whichmodel == 3 and p1 < q ):
-
-        whichmodel = 2
-
-    ###############################################################################
-
-    if ( whichmodel == 0  ):
-
-        # build (sub-basis) Lagrange polynomials (p1 = q)
-        # (QZ and RZ are the factors of Z = M')
-        
-        Lj = reshape(dot(QZ, linalg.solve( RZ.transpose(), vzeros)).transpose(),(1,p1))
-                      
-#        P = reshape(dot(QZ, linalg.solve( RZ.transpose(),reshape(fY,(p1,1)))),(1,p1))
-
-    elif ( whichmodel == 1 ):
-
-        # build mixed Lagrange polynomials:
-        # Minimum Frobenius norm Lagrange polynomials (p1 <= q) or
-        # L2-norm Lagrange polynomials (p1 == n+1 or p1 == q )
-
-        if ( p1 == n+1 or p1 == q ):
-
-            # build linear/quadratic Lagrange polynomials
-
-            Lj = linalg.solve( RZ,  dot(QZ.transpose(), vzeros) ).transpose()
-
-            if ( p1 == n+1 ):
-                Lj[n+1:q+1] = 0
-
-            Lj = reshape(Lj,(1,q))
-
+    n,p1=size_(Y,nargout=2)
+    q=((n + 1) * (n + 2)) / 2
+    if (whichmodel == 3 and p1 < q):
+        whichmodel=2
+    if (whichmodel == 0):
+        Lj=(QZ * (numpy.linalg.solve(RZ.T,[[zeros_(j - 1,1)],[1],[zeros_(p1 - j,1)]]))).T
+    else:
+        if (whichmodel == 1):
+            if (p1 == n + 1 or p1 == q):
+                Lj=(numpy.linalg.solve(RZ,QZ.T) * [[zeros_(j - 1,1)],[1],[zeros_(p1 - j,1)]]).T
+                if (p1 == n + 1):
+                    Lj[n + 2:q]=0
+            else:
+                if (shift_Y):
+                    xbase=Y[:,1]
+                    scaleY=0
+                    for i in arange_(1,p1).reshape(-1):
+                        Y[:,i]=Y[:,i] - xbase
+                        scaleY=max_(scaleY,norm_(Y[:,i]))
+                    Y=Y / scaleY
+                rhs=matlabarray([[zeros_(j - 1,1)],[1],[zeros_(p1 + n + 1 - j,1)]])
+                mualpha=(numpy.linalg.solve(RZ,(QZ.T * rhs))).T
+                Lj[1:n + 1]=mualpha[p1 + 1:p1 + n + 1].T
+                M=bcdfo_evalZ_(Y,q).T
+                Lj[n + 2:q]=M[:,n + 2:q].T * mualpha[1:p1].T
         else:
-
-            # build Minimum Frobenius-norm Lagrange polynomials (p1 <= q)
-
-            #  If shifting is active, the matrix of the interpoaltion points is first
-            #  shifted and scaled (to compute M correctly).
-
-            Y2 = copy( Y )
-            if ( shift_Y ):
-                xbase  = Y2[:,1]
-                scaleY = 0
-                for i in range(0,p1):
-                    Y2[:,i] = Y2[:,i] - xbase
-                    scaleY = max( scaleY, linalg.norm( Y2[:,i] ) )
-
-            Y2     = Y2 / scaleY;
-
-            # compute the right-hand side of the system
-
-            rhs = zeros((j-1,1))
-            rhs.append( 1 )
-            rhs.append( zeros((p1+n+1-j,1)) )
-
-            mualpha    = linalg.solve( RZ, dot( QZ.transpose(), rhs ) ).transpose()
-
-            # constant and linear part of P
-
-            Lj[0:n+1] = mualpha[p1:p1+n+1].transpose()
-
-            # quadratic part of P
-
-            M        = bcdfo_evalZ( Y2, q ).transpose();
-            Lj[n+1:q] = dot(M[:,n+1:q].transpose(), mualpha[0:p1].transpose())     
-
-    elif ( whichmodel == 2 ):
-
-        # build Minimum L2 norm Lagrange polynomials (p1 <= q)
-        # (QZ and RZ are the factors of Z = M')
-        
-        if ( p1 < q ):
-            Lj = dot( QZ, linalg.lstsq( RZ.transpose(), vzeros) ).transpose()
-        else:
-            Lj = dot( QZ, linalg.solve( RZ.transpose(), vzeros) ).transpose()
-
-    elif ( whichmodel == 3 ):
-
-        # build Regression Lagrange polynomials (p1 >= q)
-        # (QZ and RZ are the factors of Z = M)
-
-        if ( p1 > q ):
-            Lj = linalg.lstsq( RZ,  dot(QZ.transpose(), vzeros) ).transpose()
-        else:
-            Lj = linalg.solve( RZ,  dot(QZ.transpose(), vzeros) ).transpose()
-
-    elif ( whichmodel == 8 ):
-
-        if ( p1 < q ):
-            Lj = dot( QZ, linalg.lstsq( RZ.transpose(), vzeros) ).transpose()
-        else:
-            Lj = dot( QZ, linalg.solve( RZ.transpose(), vzeros) ).transpose()
-
-    elif ( whichmodel == 9 ):
-
-        # build gmodel Lagrange polynomials
-        # (for the interpolation matrix without the gradient)
-
-        if ( p1 < q ):
-            Lj = dot( QZ, linalg.lstsq( RZ.transpose(), vzeros) ).transpose()
-        else:
-            Lj = dot( QZ, linalg.solve( RZ.transpose(), vzeros) ).transpose()
-
+            if (whichmodel == 2):
+                if (p1 < q):
+                    Lj=(QZ * (pinv_(RZ.T) * [[zeros_(j - 1,1)],[1],[zeros_(p1 - j,1)]])).T
+                else:
+                    Lj=(QZ * (numpy.linalg.solve(RZ.T,[[zeros_(j - 1,1)],[1],[zeros_(p1 - j,1)]]))).T
+            else:
+                if (whichmodel == 3):
+                    Lj=(pinv_(RZ) * QZ.T * [[zeros_(j - 1,1)],[1],[zeros_(p1 - j,1)]]).T
     return Lj
---
+++ bcdfo_computeP_Anke.py
@@ -0,0 +1,213 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 11 15:58:53 2014
+
+@author: jaco_da
+"""
+import numpy as np
+from bcdfo_evalZ import *
+
+import helper
+from runtime import matlabarray
+
+#@helper.convertingDecorator
+#def bcdfo_computeP_(QZ, RZ, Y, fY, whichmodel, P_old, ind_Y,
+#         i_xold, i_xplus, g, scale, shift_Y, Delta0=None, indfree=None, gmat=None, hessian=None, 
+#         epsilon=None, noisy=None):
+#										
+#    return bcdfo_computeP(QZ, RZ, Y, fY, whichmodel, P_old, ind_Y,
+#         i_xold, i_xplus, g, scale, shift_Y, Delta0=None)#, indfree=None, gmat=None, hessian=None, 
+#         #epsilon=None, noisy=None)
+
+@helper.convertingDecorator
+def bcdfo_computeP_(QZ, RZ, Y, fY, whichmodel, P_old, ind_Y,
+         i_xold, i_xplus, g, scale, shift_Y, Delta0=None, indfree=None, gmat=None, hessian=None, 
+         epsilon=None, noisy=None):
+		
+#		QZ = helper.convert(QZ)
+#		RZ = helper.convert(RZ)
+#		Y = helper.convert(Y)
+#		#print "fY", fY
+#		#print "fY[1,:]", fY[1,:]
+#		#print "type fY1,:", type(fY[1,:])
+#		#for lst in fY:
+#	#		break
+#		#lst = [gnlph for gnlph in fY[1, :]]
+#	#	print "gnlph lst:", lst
+#		fY = helper.convertArray(fY)#lst)
+#		whichmodel = helper.convert(whichmodel)
+#		P_old = helper.convert(P_old)
+#		ind_Y = helper.convert(ind_Y)
+#		i_xold = helper.convert(i_xold)
+#		i_xplus = helper.convert(i_xplus)
+#		g = helper.convert(g)
+#		scale = helper.convert(scale)
+#		shift_Y = helper.convert(shift_Y)
+#		Delta0 = helper.convert(Delta0)
+#		indfree = helper.convert(indfree)
+#		gmat = helper.convert(gmat)
+#		hessian = helper.convert(hessian) 
+#		epsilon = helper.convert(epsilon)
+#		noisy = helper.convert(noisy)
+#		
+#		P = bcdfo_computeP(QZ, RZ, Y, fY, whichmodel, P_old, ind_Y,
+#			i_xold, i_xplus, g, scale, shift_Y, Delta0)#), indfree, gmat, hessian, 
+#		      #epsilon, noisy)
+#				
+#		P = matlabarray(P)
+						
+		return bcdfo_computeP(QZ, RZ, Y, fY, whichmodel, P_old, ind_Y,
+			i_xold, i_xplus, g, scale, shift_Y, Delta0)#), indfree, gmat, hessian, 
+#		      epsilon, noisy)
+
+#return P
+
+
+def bcdfo_computeP( QZ, RZ, Y, fY, whichmodel, P_old, ind_Y, i_xold, i_xplus, g, scale, shift_Y, Delta0 ):
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#%
+#%  Computes the polynomial P, where P is then represented by a row vector
+#%  containing its coefficients for the successive monomials.
+#%  More specifically, these values are:
+#%  P(1)        : constant coefficient,
+#%  P(2:n+1)    : coefficients for the linear terms in x(1)... x(n),
+#%  P(n+2:2n+2) : coefficients for the squared terms in x(1)^2 ... x(n)^2
+#%  P(2n+3,3n+2): coefficients for the quadratic terms of the first subdiagonal:
+#%                in x(1)*x(2) ... x(n-1)*x(n)
+#%  P(3n+3,4n+1): coefficients for the quadratic terms of the second subdiagonal:
+#%                in x(1)*x(3) ... x(n-2)*x(n)
+#%  etc.
+#%
+#%  INPUTS:
+#%
+#%  QZ, RZ      : the QR factors of the (possibly shifted) matrix Z containing
+#%                the polynomial expansion of the interpolation points
+#%  Y           : current interpolation set
+#%  fY          : function values of the interpolation points
+#%  whichmodel  : the kind of model to build
+#%  P_old       : polynomial coefficients of the former polynomial
+#%  scale       : scaling of the interpolation matrix
+#%  shift_Y     : shift of the matrix of interpolation points
+#%
+#%  OUTPUT:
+#%
+#%  P           : a row vector containing the coefficients of the polynomial
+#%
+#%  DEPENDENCIES: -
+#%
+#%  PROGRAMMING: A. Troeltzsch, September 2010.
+#%
+#%  TEST:
+#%  Y = [ 0 1 0 2 0 ; 0 0 1 0 2 ]; fY = [ 1 2 3 4 5 ];
+#%  whichmodel = 0;
+#%  [ QZ, RZ, xbase, scale ] = bcdfo_build_QR_of_Y(  Y, whichmodel, 1 );
+#%  P = bcdfo_computeP( QZ, RZ, Y, fY, whichmodel, [0, 0, 0, 0, 0], 0, ...
+#%      1, 1, [0, 0], scale, 1, 1 )
+#%  should give:
+#%  P =
+#%   1.0000   1.0000   4.0000   4.0000       0
+#%
+#%  CONDITIONS OF USE: Use at your own risk! No guarantee of any kind given.
+#%
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+	[n,p1] = Y.shape#size(Y)
+	#badcond = 0
+	q = ( ( n + 1 ) * ( n + 2 ) ) / 2
+
+	if ( whichmodel == 3 and p1 < q ):
+		#  %  for underdetermined regression model use min l2-norm model
+		whichmodel = 2
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+	if ( whichmodel == 0 ):
+
+		   #% build (sub-basis) model (p1 = q) 
+		   #% (QZ and RZ are the factors of Z = M')
+
+		   #warning off
+
+		P        = np.dot( QZ , np.linalg.solve( RZ.T , fY.T ) ).T
+
+	elif ( whichmodel == 1 ):
+
+		#%  build mixed model: Minimum Frobenius norm model (p1 <= q)
+		#%  and l2-norm model (p1 == n+1 or p1 == q)
+   
+		n_rhs = fY.shape[0]#size(fY,1)
+
+		if ( p1 == n+1 or p1 == q ):
+      
+			P[0:n_rhs-1,0:p1-1] = np.linalg.solve( RZ , np.dot( QZ.T , fY.T ) ).T
+
+			if ( p1 == n+1 ):
+				P[0:n_rhs-1,n+1:q-1] = 0.0
+		else:
+
+			#%  build Minimum Frobenius norm model (p1 <= q)
+			#%  minimizing the norm of the Hessian
+			#%  (QZ and RZ are the factors of F = [MQMQ' ML; ML' 0])
+	
+			#%  If shifting is active, the matrix of the interpoaltion points is first
+			#%  shifted and scaled (to compute M correctly).
+
+			if ( shift_Y ):
+				xbase  = Y[:,0]
+				scaleY = 0
+				for i in range(p1):
+					Y[:,i] = Y[:,i] - xbase
+					scaleY = max( scaleY, np.linalg.norm( Y[:,i] ) )
+
+				Y     = Y / scaleY
+      
+
+	      #% compute right-hand side with function values and zero
+			P = []
+			for i in range(n_rhs):
+				rhs = fY[i,:].append( np.zeros((1,n+1)))
+
+				mualpha    = np.linalg.solve( RZ , np.dot( QZ.T , rhs.T ) ).T
+
+				#% constant and linear part of P
+
+				P_i[0:n] = mualpha[p1:p1+n].T
+
+				#% quadratic part of P
+
+				M        = bcdfo_evalZ( Y, q ).T
+				P_i[n+1:q-1] = np.dot(M[ :, n+2 : q ].T , mualpha[ 1: p1 ].T)
+         
+				P = P.append(P_i, axis=1)
+         
+	elif ( whichmodel == 2 ):
+		
+
+		#% Minimum L2 norm model (p1 <= q)
+		#% (QZ and RZ are the factors of Z = M')
+		#% Take pseudo-inverse for underdetermined system because the result
+		#% is different from using backslash-operator
+   
+#%   if ( p1 < q )
+
+#%      warning off
+#%      P        = ( QZ * ( pinv(RZ') * fY' ) )';
+
+#%   else
+
+#      warning off
+		P        = np.dot( QZ , np.linalg.solve( RZ.T , fY.T ) ).T
+
+#%   end
+   
+	elif ( whichmodel == 3 ):
+
+		#% build Regression model (p1 >= q)
+		#% (QZ and RZ are the factors of Z = M)
+		#% Take pseudo-inverse for solving the system because the result
+		#% is different from using backslash-operator (except for p1==q)
+
+#   warning off
+		P        = np.dot(np.dot( np.linalg.pinv(RZ) ,  QZ.T) , fY.T ).T
+
+
+	return P
