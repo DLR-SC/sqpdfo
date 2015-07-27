ea4528574f80a1f40409f10a1c37f647d2c86183
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-27 11:08:19 +0200

    Addition of copy_ in ecdfo_swap_in_Y, and correction of the tests
    Tests were not correct, but the function was (though copy_ were missing)

--- tests/ecdfo_swap_in_Y_test.py
+++ tests/ecdfo_swap_in_Y_test.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 """
+
 Created on Fri Nov 14 18:53:38 2014
-%  TEST:
-%
+% TESTS :
+
 %  Y = [ 0 1 0 2 1 0 ; 0 0 1 0 0.01 2 ]; ind_Y = [1 2 3 4 5 6];
 %  fY = [2 5 1 3 2 6];
 %  [QZ,RZ,xbase,scale] = bcdfo_build_QR_of_Y( Y, 0, 0, 1, 1, 1e15 );
 %  Z = QZ*RZ;
-%  [ QZ, RZ, Y, xbase, scale ] = bcdfo_swap_in_Y( 1, 3, QZ, RZ, Y, ind_Y, ...
-%     fY, xbase, 0, scale, 0, 1, 1, 1e15 )
-%  [ QZ, RZ, Y, xbase, scale ] = bcdfo_swap_in_Y( 1, 3, QZ, RZ, Y, ind_Y, ...
-%     fY, xbase, 0, scale, 0, 1, 1, 1e15 )
+%  [ QZ, RZ, Y, xbase, scale ] = ecdfo_swap_in_Y( 1, 3, QZ, RZ, Y, ind_Y, ...
+%     fY, [],[],xbase, 0, scale, 0, 1, 1, 1e15 )
+%  [ QZ, RZ, Y, xbase, scale ] = ecdfo_swap_in_Y( 1, 3, QZ, RZ, Y, ind_Y, ...
+%     fY, [],[],xbase, 0, scale, 0, 1, 1, 1e15 )
 %  norm( Z - QZ*RZ)
 %  should give something very small.
 %
@@ -19,14 +20,15 @@ Created on Fri Nov 14 18:53:38 2014
 %  fY = [2 5 1 3 2 6];
 %  [QZ,RZ,xbase,scale] = bcdfo_build_QR_of_Y( Y, 0, 1, 1, 1, 1e15 );
 %  Z = QZ*RZ;
-%  [ QZ, RZ, Y, xbase, scale ] = bcdfo_swap_in_Y( 1, 3, QZ, RZ, Y, ind_Y, ...
-%     fY, xbase, 0, scale, 1, 1, 1, 1e15 )
-%  [ QZ, RZ, Y, xbase, scale ] = bcdfo_swap_in_Y( 1, 3, QZ, RZ, Y, ind_Y, ...
-%     fY, xbase, 0, scale, 1, 1, 1, 1e15 )
+%  [ QZ, RZ, Y, xbase, scale ] = ecdfo_swap_in_Y( 1, 3, QZ, RZ, Y, ind_Y, ...
+%     fY, [], [], xbase, 0, scale, 1, 1, 1, 1e15 )
+%  [ QZ, RZ, Y, xbase, scale ] = ecdfo_swap_in_Y( 1, 3, QZ, RZ, Y, ind_Y, ...
+%     fY, [],[],xbase, 0, scale, 1, 1, 1, 1e15 )
 %  norm( Z - QZ*RZ)
 @author: jaco_da
 """
-
+import sys
+sys.path.append("../")
 import unittest
 from ecdfo_swap_in_Y import *
 from runtime import *
@@ -36,45 +38,48 @@ from bcdfo_build_QR_of_Y import *
 #import helper
 
 class Test_ecdfo_swap_in_Y(unittest.TestCase):
+    """
+      Reminder :
+      This class is a test for ecdfo_swap_in_Y which swaps the position of interpolation points i and j in Y, and updates the
+     factorization of Z(Y) accordingly.
+    """ 
+    def setUp(self):
+        self.Y = matlabarray([[ 0, 1, 0, 2, 1, 0], [0, 0, 1, 0, 0.01, 2 ]])
+        self.ind_Y = matlabarray([1, 2, 3, 4, 5, 6])
+        self.fY = matlabarray([2, 5, 1, 3, 2, 6])
+        #self.options = helper.dummyOptions()
+        #self.values = helper.dummyValues()
 
-	def setUp(self):
-		self.Y = matlabarray([[ 0, 1, 0, 2, 1, 0], [0, 0, 1, 0, 0.01, 2 ]])
-		self.ind_Y = matlabarray([1, 2, 3, 4, 5, 6])
-		self.fY = matlabarray([2, 5, 1, 3, 2, 6])
-		#self.options = helper.dummyOptions()
-		#self.values = helper.dummyValues()
+        pass
 
-		pass
-
-	def test_ecdfo_swap_in_Y(self):
-
-		#self.Y = np.array([[ 0, 1, 0, 2, 1, 0], [0, 0, 1, 0, 0.01, 2 ]])
-		QZ,RZ,xbase,scale = bcdfo_build_QR_of_Y_( self.Y, 0, 0, 1, 1, 1e15 )
-		Z = QZ*RZ
-		
-		#print "QZ", QZ
-		#print "type(QZ)", type(QZ)
-		#print "RZ", RZ
-		#print "type(RZ)", type(RZ)
-		#print "Z", Z
-		#print "xbase", xbase
-		#print "type(xbase)", type(xbase)		
-		#self.fY, matlabarray(xbase), 0, matlabarray(scale), 0, 1, 1, 1e15 )
-
-		#print "scale", scale
-		#print "tyoe(scale)", type(scale)
-		
-		#QZ, RZ, Y, xbase, scale = ecdfo_swap_in_Y_( 1, 3, matlabarray(QZ), matlabarray(RZ), self.Y, self.ind_Y,
-		ceY = self.Y
-		ciY = self.Y
-		QZ,RZ,Y,ind_Y,fY,ciY,ceY,xbase,scale = ecdfo_swap_in_Y_( 1, 3, QZ, RZ, self.Y, self.ind_Y,
-		self.fY, ciY, ceY, xbase, 0, scale, 0, 1, 1, 1e15 )
-		ret = norm_( Z - QZ*RZ)		
-		self.assertTrue(ret < 2.0)
-		
-		#ecdfo_swap_in_Y_(i=None,j=None,QZ=None,RZ=None,Y=None,ind_Y=None,fY=None,
-		#ciY=None,ceY=None,xbase=None,whichmodel=None,scale=None,shift_Y=None,Delta=None,
-		#normgx=None,kappa_ill=None)
+    def test_ecdfo_swap_in_Y(self):
+        
+        """
+            Matlab test
+        """
+        QZ,RZ,xbase,scale = bcdfo_build_QR_of_Y_( self.Y, 0, 0, 1, 1, 1e15 )
+        Z = QZ*RZ
+#        
+        ceY = matlabarray([])
+        ciY = matlabarray([])
+        QZ,RZ,Y,ind_Y,fY,ciY,ceY,xbase,scale = ecdfo_swap_in_Y_( 1, 3, QZ, RZ, self.Y, self.ind_Y,
+        self.fY, ciY, ceY, xbase, 0, scale, 0, 1, 1, 1e15 )
+        QZ,RZ,Y,ind_Y,fY,ciY,ceY,xbase,scale = ecdfo_swap_in_Y_( 1, 3, QZ, RZ, Y, ind_Y,
+        fY, ciY, ceY, xbase, 0, scale, 0, 1, 1, 1e15 )
+        ret = norm_( Z - QZ*RZ)  
+        self.assertTrue(ret < 1e-15)
+#        
+#        #Scaled version :
+        QZ,RZ,xbase,scale = bcdfo_build_QR_of_Y_( self.Y, 0, 1, 1, 1, 1e15 )
+        Z = QZ*RZ
+        ceY = matlabarray([])
+        ciY = matlabarray([])
+        QZ,RZ,Y,ind_Y,fY,ciY,ceY,xbase,scale = ecdfo_swap_in_Y_( 1, 3, QZ, RZ, self.Y, self.ind_Y,
+        self.fY, ciY, ceY, xbase, 0, scale, 1, 1, 1, 1e15 )
+        QZ,RZ,Y,ind_Y,fY,ciY,ceY,xbase,scale = ecdfo_swap_in_Y_( 1, 3, QZ, RZ, Y, ind_Y,
+        fY, ciY, ceY, xbase, 0, scale, 1, 1, 1, 1e15 )
+        ret = norm_( Z - QZ*RZ)  
+        self.assertTrue(ret < 1e-15)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
