1abca48cccb9b765f9685edf430a99243ba20385
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-27 08:55:02 +0200

    Addition of some comments, small modification of the test

--- tests/evalfgh_test.py
+++ tests/evalfgh_test.py
@@ -44,7 +44,8 @@ ans =
 
 @author: jaco_da
 """
-
+import sys
+sys.path.append("../")
 import unittest
 from evalfgh import *
 from ecdfo_func import *
@@ -52,35 +53,41 @@ from ecdfo_func import *
 #import helper
 
 class Test_evalfgh(unittest.TestCase):
+    """
+      Reminder :
+      This class is a test for evalfgh
+    """ 
+    def setUp(self):
+        self.key = 2
+        self.xy = matlabarray([-2, 2, 2, 1, 1]).T
+        #self.options = helper.dummyOptions()
+        #self.values = helper.dummyValues()
+        
+        set_simul_not_initialized(0)
+        set_prob(5)
 
-	def setUp(self):
-		self.key = 2
-		self.xy = matlabarray([-2, 2, 2, 1, 1]).T
-		#self.options = helper.dummyOptions()
-		#self.values = helper.dummyValues()
-		
-		set_simul_not_initialized(0)
-		set_prob(5)
+    def test_evalfgh(self):
+        """
+        Test with some values, results compared with matlab
+        """
+        msg,out2,out3,out4 = evalfgh_(self.key,self.xy,lm=None)
 
-	def test_evalfgh(self):
-		msg,out2,out3,out4 = evalfgh_(self.key,self.xy,lm=None)
-
-		#print "msg,out2,out3,out4", msg,out2,out3,out4
-		
-		correctmsg = 0
-		correctout2 = 3.354626279025119e-04
-		correctout3 =  matlabarray([])
-		correctout4 =    matlabarray([4, -1, 1]).T
-		
-		self.assertEqual(correctmsg, msg)
-		self.assertAlmostEqual(correctout2, out2, 8)
-		self.assertEqual(correctout3, out3)
-		
-		self.assertTrue((abs(correctout4 - out4) < 1e-7).all())
-		
-		#self.assertEqual(correctout4, out4)
+        #print "msg,out2,out3,out4", msg,out2,out3,out4
+        
+        correctmsg = 0
+        correctout2 = 3.354626279025119e-04
+        correctout3 =  matlabarray([])
+        correctout4 =    matlabarray([4, -1, 1]).T
+        
+        self.assertEqual(correctmsg, msg)
+        self.assertAlmostEqual(correctout2, out2, places=15)
+        self.assertEqual(correctout3, out3)
+        
+        self.assertTrue(compare_matlabarray(correctout4, out4, 1e-15, 1e-15))
+        
+        #self.assertEqual(correctout4, out4)
 
 
 if __name__ == '__main__':
-	unittest.main()
-	
+    unittest.main()
+    
