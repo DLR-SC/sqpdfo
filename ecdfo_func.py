7bbabcf04d2f38851e5103eead8b2ac9e585be46
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-28 10:02:20 +0200

    Correction of ecdfo_func
    This correction had to be done in order to get a column vector. Before modification, the transpose did not change anything
    (matlabarray([2,3]).T == matlabarray([2,3]),
    but (matlabarray([[2,3]]).T == matlabarray([[2],[3]])

--- ecdfo_func.py
+++ ecdfo_func.py
@@ -42,19 +42,19 @@ def ecdfo_func_(x=None,*args,**kwargs):
         f=x[1] ** 2 + x[2] ** 2 + x[3] ** 2
         ce[1]=x[1] + x[2] + x[3]
         ce[2]=x[1] + 2 * x[2] + 3 * x[3] - 1
-        ce=ce.T
+        ce=matlabarray(ce).T
     elif prob == 4:
         f=x[1] ** 2 + x[2] ** 2 + x[3] ** 2 + x[4]
         ce[1]=x[1] + x[2] + x[3]
         ce[2]=x[1] + 2 * x[2] + 3 * x[3] - 1
         ce[3]=x[4] ** 3 - 1
-        ce=ce.T
+        ce=matlabarray(ce).T
     elif prob == 5:
         f=exp_(x[1] * x[2] * x[3] * x[4] * x[5])
         ce[1]=x[1] ** 2 + x[2] ** 2 + x[3] ** 2 + x[4] ** 2 + x[5] ** 2 - 10
         ce[2]=x[2] * x[3] - 5 * x[4] * x[5]
         ce[3]=x[1] ** 3 + x[2] ** 3 + 1
-        ce=ce.T
+        ce=matlabarray(ce).T
     msg=0
     return msg,f,ci,ce
     
