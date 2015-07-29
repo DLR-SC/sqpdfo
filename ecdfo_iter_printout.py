98351b8f91cb4e3a0dd586e3463e25a97196c0d6
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-07-29 09:40:38 +0200

    Commented some printout code in ecdfo_iter_printout
    This code is for the time being not lisible and is not useful for the tests I am doing

--- ecdfo_iter_printout.py
+++ ecdfo_iter_printout.py
@@ -21,20 +21,20 @@ def ecdfo_iter_printout_(info=None,old_delta=None,norms=None,pc=None,itype=None,
 #    varargin = cellarray(args)
 #    nargin = 11-[info,old_delta,norms,pc,itype,values,nb,mi,options,constrained_pbl,merit].count(None)+len(args)
 
-    if options.verbose < 4:
-        fprintf_(options.fout,char('%4i  %4i'),info.niter,info.nsimul[2] + info.nsimul[4])
-        fprintf_(options.fout,char('  %+14.8e  %+14.8e  '),info.f,merit)
-        if constrained_pbl:
-            fprintf_(options.fout,char('%10.4e  %14.8e'),info.glagn,info.feasn)
-        else:
-            fprintf_(options.fout,char('%10.4e'),info.glagn)
-        if info.niter > 1:
-            fprintf_(options.fout,char('  %8.2e'),old_delta)
-            fprintf_(options.fout,char('  %8.2e'),norms)
-            fprintf_(options.fout,char('  %4.2f'),pc)
-            fprintf_(options.fout,char('  %5s\\n'),itype)
-        else:
-            fprintf_(options.fout,char('  \\n'))
+#    if options.verbose < 4:
+#        fprintf_(options.fout,char('%4i  %4i'),info.niter,info.nsimul[2] + info.nsimul[4])
+#        fprintf_(options.fout,char('  %+14.8e  %+14.8e  '),info.f,merit)
+#        if constrained_pbl:
+#            fprintf_(options.fout,char('%10.4e  %14.8e'),info.glagn,info.feasn)
+#        else:
+#            fprintf_(options.fout,char('%10.4e'),info.glagn)
+#        if info.niter > 1:
+#            fprintf_(options.fout,char('  %8.2e'),old_delta)
+#            fprintf_(options.fout,char('  %8.2e'),norms)
+#            fprintf_(options.fout,char('  %4.2f'),pc)
+#            fprintf_(options.fout,char('  %5s\\n'),itype)
+#        else:
+#            fprintf_(options.fout,char('  \\n'))
     if options.verbose >= 4:
         fprintf_(options.fout,char('%s\\n'),values.dline)
         fprintf_(options.fout,char('iter %i,'),info.niter)
