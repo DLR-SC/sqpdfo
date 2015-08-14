9b122f4e4ecda1eb7a92d5b0013291fb3d411ccf
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-08-14 15:28:40 +0200

    Suppression of matlabarrays in bcdfo_computeLj

--- bcdfo_computeLj.py
+++ bcdfo_computeLj.py
@@ -11,6 +11,8 @@ try:
 except ImportError:
     from smop.runtime import *
 from bcdfo_evalZ import *
+from copy import copy
+from numpy import array
 
 def bcdfo_computeLj_(QZ=None,RZ=None,j=None,Y_=None,whichmodel=None,scale=None,shift_Y=None,*args,**kwargs):
 #    varargin = cellarray(args)
@@ -18,42 +20,43 @@ def bcdfo_computeLj_(QZ=None,RZ=None,j=None,Y_=None,whichmodel=None,scale=None,s
 
     #Copy only if necessary
     if (shift_Y):
-        Y=copy_(Y_)
+        Y=copy(Y_)
     else:
         Y=Y_
 
+    j=j+1
     n,p1=size_(Y,nargout=2)
     q=((n + 1) * (n + 2)) / 2
     if (whichmodel == 3 and p1 < q):
         whichmodel=2
     if (whichmodel == 0):
-        Lj=(QZ * (numpy.linalg.solve(RZ.T,concatenate_([zeros_(j - 1,1),matlabarray([[1]]),zeros_(p1 - j,1)],axis=0)))).T
+        Lj=(QZ.dot((numpy.linalg.solve(RZ.T,concatenate_([zeros_(j - 1,1),array([[1]]),zeros_(p1 - j,1)],axis=0))))).T
     else:
         if (whichmodel == 1):
             if (p1 == n + 1 or p1 == q):
-                Lj=(numpy.linalg.solve(RZ,QZ.T) * concatenate_([zeros_(j - 1,1),matlabarray([[1]]),zeros_(p1 - j,1)],axis=0)).T
+                Lj=(numpy.linalg.solve(RZ,QZ.T).dot(concatenate_([zeros_(j - 1,1),array([[1]]),zeros_(p1 - j,1)],axis=0))).T
                 if (p1 == n + 1):
-                    Lj[n + 2:q]=0
+                    Lj[n + 1:q]=0
             else:
                 if (shift_Y):
-                    xbase=Y[:,1]
+                    xbase=copy(Y[:,0])
                     scaleY=0
-                    for i in arange_(1,p1).reshape(-1):
+                    for i in range(0,p1):
                         Y[:,i]=Y[:,i] - xbase
                         scaleY=max_(scaleY,norm_(Y[:,i]))
                     Y=Y / scaleY
-                rhs=concatenate_([zeros_(j - 1,1),matlabarray([[1]]),zeros_(p1 + n +1 - j,1)],axis=0)
-                mualpha=(numpy.linalg.solve(RZ,(QZ.T * rhs))).T
-                Lj[1:n + 1]=mualpha[p1 + 1:p1 + n + 1].T
+                rhs=concatenate_([zeros_(j - 1,1),array([[1]]),zeros_(p1 + n +1 - j,1)],axis=0)
+                mualpha=(numpy.linalg.solve(RZ,(QZ.T.dot(rhs)))).T
+                Lj[0:n + 1]=mualpha[p1:p1 + n + 1].T
                 M=bcdfo_evalZ_(Y,q).T
-                Lj[n + 2:q]=M[:,n + 2:q].T * mualpha[1:p1].T
+                Lj[n + 1:q]=M[:,n + 1:q].T.dot( mualpha[0:p1]).T
         else:
             if (whichmodel == 2):
                 if (p1 < q):
-                    Lj=(QZ * (pinv_(RZ.T) * concatenate_([zeros_(j - 1,1),matlabarray([[1]]),zeros_(p1 - j,1)],axis=0))).T
+                    Lj=(QZ.dot((pinv_(RZ.T).dot(concatenate_([zeros_(j - 1,1),array([[1]]),zeros_(p1 - j,1)],axis=0))))).T
                 else:
-                    Lj=(QZ * (numpy.linalg.solve(RZ.T,concatenate_([zeros_(j - 1,1),matlabarray([[1]]),zeros_(p1 - j,1)],axis=0)))).T
+                    Lj=(QZ.dot((numpy.linalg.solve(RZ.T,concatenate_([zeros_(j - 1,1),array([[1]]),zeros_(p1 - j,1)],axis=0))))).T
             else:
                 if (whichmodel == 3):
-                    Lj=(pinv_(RZ) * QZ.T * concatenate_([zeros_(j - 1,1),matlabarray([[1]]),zeros_(p1 - j,1)],axis=0)).T
+                    Lj=(pinv_(RZ).dot( QZ.T.dot( concatenate_([zeros_(j - 1,1),array([[1]]),zeros_(p1 - j,1)],axis=0)))).T
     return Lj
--- tests/bcdfo_computeLj_test.py
+++ tests/bcdfo_computeLj_test.py
@@ -10,9 +10,10 @@ sys.path.append("../")
 import unittest
 from bcdfo_computeLj import *#bcdfo_computeLj_
 from bcdfo_build_QR_of_Y import *#bcdfo_build_QR_of_Y_
-from runtime import matlabarray, compare_matlabarray
+from runtime import compare_array
 from bcdfo_evalP import bcdfo_evalP_
 import numpy as np
+from numpy import array
 from random import random
 #import helper
 
@@ -34,16 +35,16 @@ class Test_bcdfo_computeLj(unittest.TestCase):
         and 0 otherwise
         """
         for k in range(0,20):
-            x1_coord=[(random()-0.5)*100 for p in range(0,5)]
-            x2_coord=[(random()-0.5)*100 for p in range(0,5)]
-            Y = matlabarray([x1_coord, x2_coord])
+            x1_coord=[(random()-0.5)*100 for p in range(0,6)]
+            x2_coord=[(random()-0.5)*100 for p in range(0,6)]
+            Y = array([x1_coord, x2_coord])
             whichmodel = 0
             QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_(  Y, whichmodel, 1, 1, 1, 1e15 )
-            for i in range(1,6):
+            for i in range(0,6):
                 L= bcdfo_computeLj_( QZ, RZ, i, Y, whichmodel, scale, 1 )
-                for j in range(0,5):
-                    value=bcdfo_evalP_( L, matlabarray([[x1_coord[j]],[x2_coord[j]]]), xbase, scale, 1 )
-                    if i==j+1: # we have to be careful with the indices
+                for j in range(0,6):
+                    value=bcdfo_evalP_( L, array([[x1_coord[j]],[x2_coord[j]]]), xbase, scale, 1 )
+                    if i==j:
                         self.assertAlmostEqual(value, 1.0,places=10)
                     else:
                         self.assertAlmostEqual(value, 0,places=10)
@@ -52,12 +53,12 @@ class Test_bcdfo_computeLj(unittest.TestCase):
         """
         More complicated test initally written in the matlab code
         """
-        Y = matlabarray([[ 0, 1, 0, 2, 0], [0, 0, 1, 0, 2 ]])
+        Y = array([[ 0, 1, 0, 2, 0], [0, 0, 1, 0, 2 ]])
         whichmodel = 0
         QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_(  Y, whichmodel, 1, 1, 1, 1e15 )
-        L1 = bcdfo_computeLj_( QZ, RZ, 1, Y, whichmodel, scale, 1 )
-        correctL1 = matlabarray([1.0000,  -3.0000,  -3.0000,   4.0000,   4.0000])
-        self.assertTrue(compare_matlabarray(correctL1,L1, self.abs_tol, self.rel_tol))
+        L1 = bcdfo_computeLj_( QZ, RZ, 0, Y, whichmodel, scale, 1 )
+        correctL1 = array([1.0000,  -3.0000,  -3.0000,   4.0000,   4.0000])
+        self.assertTrue(compare_array(correctL1,L1, self.abs_tol, self.rel_tol))
         
 
 
