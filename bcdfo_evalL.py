af36b25a732c73ead8bc722c227db6394c24325a
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-08-14 14:54:43 +0200

    Suppression of matlabarrays in bcdfo_evalL

--- bcdfo_evalL.py
+++ bcdfo_evalL.py
@@ -12,6 +12,7 @@ except ImportError:
     
 from bcdfo_evalP import bcdfo_evalP_
 import numpy
+from copy import copy
     
 def bcdfo_evalL_(QZ=None,RZ=None,Y_=None,choice_set=None,x=None,xbase_=None,whichmodel=None,scale=None,shift_Y=None,*args,**kwargs):
 #    varargin = cellarray(args)
@@ -19,8 +20,8 @@ def bcdfo_evalL_(QZ=None,RZ=None,Y_=None,choice_set=None,x=None,xbase_=None,whic
 
     #Copy only if necessary
     if (shift_Y):
-        Y=copy_(Y_)
-        xbase=copy_(xbase_)
+        Y=copy(Y_)
+        xbase=copy(xbase_)
     else:
         Y=Y_
         xbase=xbase_        
@@ -33,33 +34,33 @@ def bcdfo_evalL_(QZ=None,RZ=None,Y_=None,choice_set=None,x=None,xbase_=None,whic
     if (whichmodel == 3 and p1 < q):
         whichmodel=2
     if (whichmodel == 0):
-        values[choice_set]=bcdfo_evalP_(I[choice_set,:] * (numpy.linalg.solve(RZ,QZ.T)),x,xbase,scale,shift_Y)
+        values[choice_set]=bcdfo_evalP_(I[choice_set,:].dot( (numpy.linalg.solve(RZ,QZ.T))),x,xbase,scale,shift_Y)
     else:
         if (whichmodel == 1):
             if (p1 == n + 1 or p1 == q):
-                values[choice_set]=bcdfo_evalP_(I[choice_set,:] * (QZ / RZ.T),x,xbase,scale,shift_Y)
+                values[choice_set]=bcdfo_evalP_(I[choice_set,:] .dot( (QZ / RZ.T)),x,xbase,scale,shift_Y)
             else:
                 if (shift_Y):
-                    xbase=Y[:,1]
+                    xbase=copy(Y[:,0])
                     scaleY=0
-                    for i in arange_(1,p1).reshape(-1):
+                    for i in range(0,p1):
                         Y[:,i]=Y[:,i] - xbase
                         scaleY=max_(scaleY,norm_(Y[:,i]))
                     Y=Y / scaleY
                 M=bcdfo_evalZ_(Y,q).T
-                MQ=M[:,n + 2:q]
+                MQ=M[:,n + 1:q]
                 if (shift_Y):
-                    phi=bcdfo_evalZ_((x - xbase) * scale[2],q)
+                    phi=bcdfo_evalZ_((x - xbase) * scale[1],q)
                 else:
                     phi=bcdfo_evalZ_(x,q)
-                values[choice_set]=[I[choice_set,:],zeros_(lc,n + 1)] * (QZ * (numpy.linalg.solve(RZ.T,[[MQ * phi[n + 2:q]],[phi[1:n + 1]]])))
+                values[choice_set]=concatenate_([  I[choice_set,:],  zeros_(lc,n + 1).dot(QZ .dot(numpy.linalg.solve(RZ.T,concatenate_([MQ.dot(phi[n + 1:q]),phi[0:n + 1]]))))],axis=1)
         else:
             if (whichmodel == 2):
                 if (p1 < q):
-                    values[choice_set]=bcdfo_evalP_(I[choice_set,:] * (pinv_(RZ) * QZ.T),x,xbase,scale,shift_Y)
+                    values[choice_set]=bcdfo_evalP_(I[choice_set,:].dot( (pinv_(RZ).dot(QZ.T))),x,xbase,scale,shift_Y)
                 else:
-                    values[choice_set]=bcdfo_evalP_(I[choice_set,:] * (numpy.linalg.solve(RZ,QZ.T)),x,xbase,scale,shift_Y)
+                    values[choice_set]=bcdfo_evalP_(I[choice_set,:].dot( (numpy.linalg.solve(RZ,QZ.T))),x,xbase,scale,shift_Y)
             else:
                 if (whichmodel == 3):
-                    values[choice_set]=bcdfo_evalP_(I[choice_set,:] * (QZ * pinv_(RZ.T)),x,xbase,scale,shift_Y)
+                    values[choice_set]=bcdfo_evalP_(I[choice_set,:].dot((QZ .dot(pinv_(RZ.T)))),x,xbase,scale,shift_Y)
     return values
--- tests/bcdfo_evalL_test.py
+++ tests/bcdfo_evalL_test.py
@@ -9,8 +9,9 @@ sys.path.append("../")
 import unittest
 from bcdfo_evalL import bcdfo_evalL_
 from bcdfo_build_QR_of_Y import bcdfo_build_QR_of_Y_
-from runtime import matlabarray, compare_matlabarray
+from runtime import compare_array
 from random import random
+from numpy import *
 #import helper
 
 class Test_bcdfo_evalL(unittest.TestCase):
@@ -32,14 +33,14 @@ class Test_bcdfo_evalL(unittest.TestCase):
      We verify here that for x being a point of the interpolant set, we have l_j(x) = 1 if the index j corresponds to the index of x in the interpolant set
     and 0 otherwise
         """
-        Y = matlabarray([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
+        Y = array([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
         QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y, 0, 1, 1,1,1e15 )
         
         #Here we choose the first point of the interpolant set
-        values = bcdfo_evalL_( QZ, RZ, Y, matlabarray(range(0,6)), matlabarray([[0],[0]]), xbase, 0, scale, 1 )
-        correctvalues = matlabarray([ [1],   [0],    [0],    [0], [0],   [0] ])
+        values = bcdfo_evalL_( QZ, RZ, Y, array(range(0,6)), array([[0],[0]]), xbase, 0, scale, 1 )
+        correctvalues = array([ [1],   [0],    [0],    [0], [0],   [0] ])
         
-        self.assertTrue(compare_matlabarray(values, correctvalues, self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_array(values, correctvalues, self.abs_tol, self.rel_tol))
         
 
     def test_bcdfo_evalL_2(self):
@@ -47,14 +48,14 @@ class Test_bcdfo_evalL(unittest.TestCase):
      We verify here that for x being a point of the interpolant set, we have l_j(x) = 1 if the index j corresponds to the index of x in the interpolant set
     and 0 otherwise
         """
-        Y = matlabarray([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
+        Y = array([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
         QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y, 0, 1, 1,1,1e15 )
         
         #Here we choose the fifth point of the interpolant set
-        values = bcdfo_evalL_( QZ, RZ, Y, matlabarray(range(1,6)), matlabarray([[1],[0.01]]), xbase, 0, scale, 1 )
-        correctvalues = matlabarray([ [0.],   [0.],    [0.],    [0.], [1.],   [0.] ])
+        values = bcdfo_evalL_( QZ, RZ, Y, array(range(1,6)), array([[1],[0.01]]), xbase, 0, scale, 1 )
+        correctvalues = array([ [0.],   [0.],    [0.],    [0.], [1.],   [0.] ])
         
-        self.assertTrue(compare_matlabarray(values, correctvalues, self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_array(values, correctvalues, self.abs_tol, self.rel_tol))
         
 
     def test_bcdfo_evalL_3(self):
@@ -62,36 +63,36 @@ class Test_bcdfo_evalL(unittest.TestCase):
      We verify here that for x being a point of the interpolant set, we have l_j(x) = 1 if the index j corresponds to the index of x in the interpolant set
     and 0 otherwise
         """
-        Y = matlabarray([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
+        Y = array([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
         QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y, 0, 1, 1,1,1e15 )
         
         #Here we choose the fourth point of the interpolant set
-        values = bcdfo_evalL_( QZ, RZ, Y, matlabarray([2, 4, 5]), matlabarray([[2],[0]]), xbase, 0, scale, 1 )
-        correctvalues = matlabarray([ [0],   [0],    [0],  [1], [0],   [0] ])
-        self.assertTrue(compare_matlabarray(values, correctvalues, self.abs_tol, self.rel_tol))
+        values = bcdfo_evalL_( QZ, RZ, Y, array([1, 3, 4]), array([[2],[0]]), xbase, 0, scale, 1 )
+        correctvalues = array([ [0],   [0],    [0],  [1], [0],   [0] ])
+        self.assertTrue(compare_array(values, correctvalues, self.abs_tol, self.rel_tol))
 
     
     def test_bcdfo_evalL_4(self):
         """
         We verify here that for x random, the sum of the values is equal to 1 when we have a enough point to have a complete basis.
         """
-        Y = matlabarray([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
+        Y = array([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
         QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y, 0, 1, 1,1,1e15 )
         
         #Here we choose a random point, and the complete interpolant set
         for i in range(0,50):
-            values = bcdfo_evalL_( QZ, RZ, Y, matlabarray(range(0,6)), matlabarray([[(random()-0.5)*100],[(random()-0.5)*100]]), xbase, 0, scale, 1 )
+            values = bcdfo_evalL_( QZ, RZ, Y, array(range(0,6)), array([[(random.random()-0.5)*100],[(random.random()-0.5)*100]]), xbase, 0, scale, 1 )
             self.assertAlmostEqual(values.sum(), 1.0,places=9) #NB : random values force us to reduce the precision 
 
     def test_bcdfo_evalL_5(self):
         """
         More complicated test initally written in the matlab code
         """
-        Y = matlabarray([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
+        Y = array([[ 0, 1, 0, 2, 1, 0],[0, 0, 1, 0, 0.01, 2 ]]) 
         QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y, 0, 1, 1,1,1e15 )
-        values = bcdfo_evalL_( QZ, RZ, Y, matlabarray(range(2,7)), matlabarray([[-1],[1]]), xbase, 0, scale, 1 )
-        correctvalues = matlabarray([ [0],   [97.0000],    [2.9900],    [1.0000], [-100.0000],   [-0.4950] ])
-        self.assertTrue(compare_matlabarray(values, correctvalues, self.abs_tol, self.rel_tol))
+        values = bcdfo_evalL_( QZ, RZ, Y, array(range(1,6)), array([[-1],[1]]), xbase, 0, scale, 1 )
+        correctvalues = array([ [0],   [97.0000],    [2.9900],    [1.0000], [-100.0000],   [-0.4950] ])
+        self.assertTrue(compare_array(values, correctvalues, self.abs_tol, self.rel_tol))
         
 
 if __name__ == '__main__':
