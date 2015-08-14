94f0b8878238303eb1d49b5c4a9bda8006649d8f
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-08-14 15:36:17 +0200

    Suppression of matlabarrays in bcdfo_compute_P

--- bcdfo_computeP.py
+++ bcdfo_computeP.py
@@ -11,6 +11,8 @@ try:
 except ImportError:
     from smop.runtime import *
 from bcdfo_evalZ import *
+from numpy import array
+from copy import copy
 
 def bcdfo_computeP_(QZ=None,RZ=None,Y_=None,fY=None,whichmodel=None,P_old=None,ind_Y=None,i_xold=None,i_xplus=None,g=None,scale=None,shift_Y=None,Delta0=None,*args,**kwargs):
 #    varargin = cellarray(args)
@@ -18,7 +20,7 @@ def bcdfo_computeP_(QZ=None,RZ=None,Y_=None,fY=None,whichmodel=None,P_old=None,i
 
     #Copy only if necessary
     if (shift_Y):
-        Y=copy_(Y_)
+        Y=copy(Y_)
     else:
         Y=Y_
 
@@ -28,36 +30,36 @@ def bcdfo_computeP_(QZ=None,RZ=None,Y_=None,fY=None,whichmodel=None,P_old=None,i
     if (whichmodel == 3 and p1 < q):
         whichmodel=2
     if (whichmodel == 0):
-        P=(QZ * (numpy.linalg.solve(RZ.T,fY.T))).T
+        P=(QZ.dot(numpy.linalg.solve(RZ.T,fY.T))).T
     else:
         if (whichmodel == 1):
             n_rhs=size_(fY,1)
             if (p1 == n + 1 or p1 == q):
-                P[1:n_rhs,1:p1]=(numpy.linalg.solve(RZ,(QZ.T * fY.T))).T
+                P[0:n_rhs,0:p1]=(numpy.linalg.solve(RZ,(QZ.T.dot(fY.T)))).T
                 if (p1 == n + 1):
-                    P[1:n_rhs,n + 2:q]=0.0
+                    P[0:n_rhs,n + 1:q]=0.0
             else:
                 if (shift_Y):
-                    xbase=Y[:,1]
+                    xbase=copy(Y[:,0])
                     scaleY=0
-                    for i in arange_(1,p1).reshape(-1):
+                    for i in range(0,p1):
                         Y[:,i]=Y[:,i] - xbase
                         scaleY=max_(scaleY,norm_(Y[:,i]))
                     Y=Y / scaleY
-                P=matlabarray([])
-                for i in arange_(1,n_rhs).reshape(-1):
+                P=array([])
+                for i in range(0,n_rhs):
                     rhs=concatenate_([fY[i,:],zeros_(1,n + 1)], axis=1)
-#                    rhs=matlabarray([fY[i,:],zeros_(1,n + 1)])
-                    mualpha=(numpy.linalg.solve(RZ,(QZ.T * rhs.T))).T
-                    P_i[1:n + 1]=mualpha[p1 + 1:p1 + n + 1].T
+#                    rhs=array([fY[i,:],zeros_(1,n + 1)])
+                    mualpha=(numpy.linalg.solve(RZ,(QZ.T.dot(rhs.T)))).T
+                    P_i[0:n + 1]=mualpha[p1:p1 + n + 1].T
                     M=bcdfo_evalZ_(Y,q).T
-                    P_i[n + 2:q]=M[:,n + 2:q].T * mualpha[1:p1].T
-                    P=concatenante_([P, P_i])
-#                    P=matlabarray([[P],[P_i]])
+                    P_i[n + 1:q]=M[:,n + 1:q].T.dot(mualpha[0:p1].T)
+                    P=concatenate_([P, P_i])
+#                    P=array([[P],[P_i]])
         else:
             if (whichmodel == 2):
-                P=(QZ * (numpy.linalg.solve(RZ.T,fY.T))).T
+                P=(QZ.dot((numpy.linalg.solve(RZ.T,fY.T)))).T
             else:
                 if (whichmodel == 3):
-                    P=(pinv_(RZ) * QZ.T * fY.T).T
+                    P=(pinv_(RZ).dot(QZ.T.dot(fY.T))).T
     return P
--- tests/bcdfo_computeP_test.py
+++ tests/bcdfo_computeP_test.py
@@ -13,7 +13,8 @@ from bcdfo_computeP import *
 from bcdfo_build_QR_of_Y import *
 from bcdfo_evalP import *
 from random import random
-from runtime import matlabarray, compare_matlabarray
+from runtime import compare_array
+from numpy import array
 #import numpy as np
 #import helper
 
@@ -37,34 +38,34 @@ class Test_bcdfo_computeP(unittest.TestCase):
             x1_coord=[(random()-0.5)*100 for p in range(0,6)]
             x2_coord=[(random()-0.5)*100 for p in range(0,6)]
             y_coord=[(random()-0.5)*100 for p in range(0,6)]
-            Y = matlabarray([x1_coord,x2_coord])
-            fY =matlabarray([y_coord])
+            Y = array([x1_coord,x2_coord])
+            fY =array([y_coord])
             whichmodel = 0
         
             QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_(Y, whichmodel, 1, 1, 1, 1e15 )
-            P = bcdfo_computeP_( QZ, RZ, Y, fY, whichmodel, matlabarray([0, 0, 0, 0, 0,0]), 0,
-                                1, 1, matlabarray([0, 0]), scale, 1, 1)
+            P = bcdfo_computeP_( QZ, RZ, Y, fY, whichmodel, array([0, 0, 0, 0, 0,0]), 0,
+                                1, 1, array([0, 0]), scale, 1, 1)
             for j in range(0,6):
-                value=bcdfo_evalP_( P, matlabarray([[x1_coord[j]],[x2_coord[j]]]), xbase, scale, 1 )
+                value=bcdfo_evalP_( P, array([[x1_coord[j]],[x2_coord[j]]]), xbase, scale, 1 )
                 self.assertAlmostEqual(value, y_coord[j],places=8)   
     
     def test_bcdfo_computeP_1(self):
         """
         More complicated test initally written in the matlab code
         """
-        Y = matlabarray([[ 0, 1, 0, 2, 0],[0, 0, 1, 0, 2]])
-        fY =matlabarray([[ 1, 2, 3, 4, 5 ]])
+        Y = array([[ 0, 1, 0, 2, 0],[0, 0, 1, 0, 2]])
+        fY =array([[ 1, 2, 3, 4, 5 ]])
         whichmodel = 0
         
         QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_(Y, whichmodel, 1, 1, 1, 1e15 )
-        #QZ = matlabarray(QZ)
-        #RZ = matlabarray(RZ)
+        #QZ = array(QZ)
+        #RZ = array(RZ)
         
-        #xbase = matlabarray(xbase)
-        #scale = matlabarray(scale)
-        P = bcdfo_computeP_( QZ, RZ, Y, fY, whichmodel, matlabarray([0, 0, 0, 0, 0]), 0,
-            1, 1, matlabarray([0, 0]), scale, 1, 1)
-        self.assertTrue(compare_matlabarray(P, matlabarray([1, 1, 4, 4, 0]), self.abs_tol, self.rel_tol))
+        #xbase = array(xbase)
+        #scale = array(scale)
+        P = bcdfo_computeP_( QZ, RZ, Y, fY, whichmodel, array([0, 0, 0, 0, 0]), 0,
+            1, 1, array([0, 0]), scale, 1, 1)
+        self.assertTrue(compare_array(P, array([1, 1, 4, 4, 0]), self.abs_tol, self.rel_tol))
 
 
 if __name__ == '__main__':
