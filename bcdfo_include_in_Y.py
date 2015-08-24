af1e0df054b57c0fccd1aaeaef04561c7dc5f8dc
Olivier Lienhard <olivier.lienhard@dlr.de>
2015-08-24 15:11:55 +0200

    Correction of indices in bcdfo_include_in_Y
    For the indices of position, matlab returns 0 when there is actually no position,but in python 0 is an index,
    therefore I dealt with it with -1 in ecdfo_main, but I did not see until tests with problem 4 that this had to be changed here too.

--- bcdfo_include_in_Y.py
+++ bcdfo_include_in_Y.py
@@ -12,7 +12,6 @@ except ImportError:
 from bcdfo_evalL import bcdfo_evalL_
 from bcdfo_replace_in_Y import bcdfo_replace_in_Y_
 from copy import copy
-from numpy import arange
 
 def bcdfo_include_in_Y_(x=None,QZ_=None,RZ_=None,Y_=None,choice_set=None,poisedness_threshold=None,criterion=None,xbase_=None,whichmodel=None,succ=None,scale_=None,shift_Y=None,Delta=None,normgx=None,kappa_ill=None,*args,**kwargs):
 #    varargin = cellarray(args)
@@ -25,16 +24,16 @@ def bcdfo_include_in_Y_(x=None,QZ_=None,RZ_=None,Y_=None,choice_set=None,poisedn
     scale=copy(scale_)
 
     if (length_(choice_set) == 0):
-        pos=0
+        pos=-1
         return QZ,RZ,Y,pos,xbase,scale
     Lvals=bcdfo_evalL_(QZ,RZ,Y,choice_set,x,xbase,whichmodel,scale,shift_Y)
     choice=find_(abs(Lvals) > poisedness_threshold)
     lc=length_(choice)
     if (lc == 0):
-        pos=0
+        pos=-1
         return QZ,RZ,Y,pos,xbase,scale
     crit_val=0
-    pos=0
+    pos=-1
     for i in range(0,lc):
         j=choice[i] # since choice is a vector column, choice[i] will return something like array([a]), and dimensions below are correct
         if (criterion == 'weighted'):
@@ -44,7 +43,7 @@ def bcdfo_include_in_Y_(x=None,QZ_=None,RZ_=None,Y_=None,choice_set=None,poisedn
                 cv=norm_(Y[:,j] - Y[:,[0]]) ** 2 * abs(Lvals[j])
         else:
             if (criterion == 'standard'):
-                cv=abs_(Lvals[j])
+                cv=abs(Lvals[j])
             else:
                 if (criterion == 'distance'):
                     if (succ == 1):
@@ -54,7 +53,7 @@ def bcdfo_include_in_Y_(x=None,QZ_=None,RZ_=None,Y_=None,choice_set=None,poisedn
         if (cv > crit_val):
             pos=copy(j)
             crit_val=copy(cv)
-    if (int(pos) == 0):
+    if (int(pos) == -1):
         return QZ,RZ,Y,int(pos),xbase,scale
     QZ,RZ,Y,xbase,scale=bcdfo_replace_in_Y_(QZ,RZ,x,Y,int(pos),xbase,whichmodel,scale,shift_Y,Delta,normgx,kappa_ill,nargout=5)
     return QZ,RZ,Y,int(pos),xbase,scale
