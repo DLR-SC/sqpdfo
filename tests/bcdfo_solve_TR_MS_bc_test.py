fb8346b2aa58adb33a775a58edbf7dfe6f479d81
Anke Troeltzsch <anke.troeltzsch@dlr.de>
2018-07-12 15:24:16 +0200

    Bugfix.

--- tests/bcdfo_solve_TR_MS_bc_test.py
+++ tests/bcdfo_solve_TR_MS_bc_test.py
@@ -10,8 +10,7 @@ import unittest
 from bcdfo_solve_TR_MS_bc import *
 from runtime import compare_array
 from numpy import array
-#import numpy as np
-#import helper
+import helper
 
 class Test_bcdfo_solve_TR_MS_bc(unittest.TestCase):
 
@@ -20,10 +19,11 @@ class Test_bcdfo_solve_TR_MS_bc(unittest.TestCase):
       This class is a test for solve_TR_MS which as its name indicates, solves the Trust Region minimization based on the More-Sorensen algorithm subject to bound  constraints
     """ 
     def setUp(self):
-        #self.options = helper.dummyOptions()
+        self.options = helper.dummyOptions()
+        self.options.verbose = 2
         #self.values = helper.dummyValues()
-        self.abs_tol=1e-14;
-        self.rel_tol=1e-14;
+        self.abs_tol=1e-14
+        self.rel_tol=1e-14
         pass
 
     def test_bcdfo_solve_TR_MS_bc_1(self):
@@ -38,7 +38,7 @@ class Test_bcdfo_solve_TR_MS_bc(unittest.TestCase):
         eps_D = 0.001
         stratLam = 1
         
-        s, lamb, norms, value, gplus, nfact, neigd, msg= bcdfo_solve_TR_MS_bc_( gx, H, lb, ub, Delta, eps_D, stratLam )
+        s, lamb, norms, value, gplus, nfact, neigd, msg= bcdfo_solve_TR_MS_bc_( gx, H, lb, ub, Delta, eps_D, stratLam, self.options )
         correctS = array( [0.515282741049029, -0.8575287994226390])
         correctgplus=array( [0.9159581676602855, 4.804052449180984])
         self.assertTrue(compare_array(correctS, s, self.abs_tol, self.rel_tol))
@@ -51,7 +51,7 @@ class Test_bcdfo_solve_TR_MS_bc(unittest.TestCase):
         self.assertEqual(str(msg), '(partly) interior solution')
         
         lb = array([[-0.1], [-0.1]])
-        s, lamb, norms, value, gplus, nfact, neigd, msg= bcdfo_solve_TR_MS_bc_( gx, H, lb, ub, Delta, eps_D, stratLam )
+        s, lamb, norms, value, gplus, nfact, neigd, msg= bcdfo_solve_TR_MS_bc_( gx, H, lb, ub, Delta, eps_D, stratLam, self.options )
         correctS = array( [-0.1, -0.1])
         correctgplus=array( [0.9159581676602855, 4.804052449180984])
         self.assertTrue(compare_array(correctS, s, self.abs_tol, self.rel_tol))
@@ -65,13 +65,13 @@ class Test_bcdfo_solve_TR_MS_bc(unittest.TestCase):
         
         lb = array([[-10], [-10]])
         ub = array([[0], [0]])
-        s, lamb, norms, value, gplus, nfact, neigd, msg= bcdfo_solve_TR_MS_bc_( gx, H, lb, ub, Delta, eps_D, stratLam )
+        s, lamb, norms, value, gplus, nfact, neigd, msg= bcdfo_solve_TR_MS_bc_( gx, H, lb, ub, Delta, eps_D, stratLam, self.options )
         correctS = array( [0, -0.6])
         correctgplus=array( [0.9159581676602855, 4.804052449180984])
         self.assertTrue(compare_array(correctS, s, self.abs_tol, self.rel_tol))
         self.assertAlmostEqual(lamb, 0, places=15)
         self.assertAlmostEqual(norms, 0.6, places=15)
-        self.assertAlmostEqual(value, -0.9, places=15)
+        self.assertAlmostEqual(double(value), -0.9, places=15)
         self.assertTrue(compare_array(correctgplus, gplus, self.abs_tol, self.rel_tol))
         self.assertEqual(nfact,9)
         self.assertEqual(neigd,0)
@@ -88,7 +88,7 @@ class Test_bcdfo_solve_TR_MS_bc(unittest.TestCase):
         delta_r=1.0
         prec_r=1e-6
         stratLam=1
-        s, lamb, norms, value, gplus, nfact, neigd, msg=bcdfo_solve_TR_MS_bc_(g1,H1,lb_r,ub_r,delta_r,prec_r,stratLam,nargout=8)
+        s, lamb, norms, value, gplus, nfact, neigd, msg=bcdfo_solve_TR_MS_bc_(g1,H1,lb_r,ub_r,delta_r,prec_r,stratLam, self.options,nargout=8)
         correctS = array([-7.217986001584447e-01,-5.625108528130431e-01,-4.032231054676398e-01])
         correctgplus=array( [5.255977819373422e+00, 8.199488197185971,1.114299857499852e+01])
         
@@ -96,9 +96,9 @@ class Test_bcdfo_solve_TR_MS_bc(unittest.TestCase):
 #        factorization during the call to bcdfo_solve_TR_MS. The last number of the matrix has its eigth digit different in python and matlab,
 #        therefore we loose so much in accuracy. But other than that, I believe that the test is correct.
         self.assertTrue(compare_array(correctS, s, 1e-6, 1e-6))
-        self.assertAlmostEqual(lamb, 3.546388415234277e-01, places=5)
+        self.assertAlmostEqual(double(lamb), 3.546388415234277e-01, places=5)
         self.assertAlmostEqual(norms, 1.000000275753019, places=6)
-        self.assertAlmostEqual(value, -6.449586510274759, places=6)
+        self.assertAlmostEqual(double(value), -6.449586510274759, places=6)
         self.assertTrue(compare_array(correctgplus, gplus, 1e-6, 1e-6))
         self.assertEqual(nfact,7)
         self.assertEqual(neigd,0)
