37a4e732f3f7447880ebf1b79fa8f7efea90792a
Anke Troeltzsch <anke.troeltzsch@dlr.de>
2018-07-16 15:07:24 +0200

    Bugfix.

--- tests/bcdfo_find_new_yj_test.py
+++ tests/bcdfo_find_new_yj_test.py
@@ -11,8 +11,8 @@ import unittest
 from bcdfo_find_new_yj import bcdfo_find_new_yj_
 from bcdfo_build_QR_of_Y import bcdfo_build_QR_of_Y_
 from runtime import compare_array
-from numpy import array
-#import helper
+from numpy import array, double
+
 
 class Test_bcdfo_find_new_yj(unittest.TestCase):
     """
@@ -21,8 +21,6 @@ class Test_bcdfo_find_new_yj(unittest.TestCase):
       and also yields the improvement of poisedness corresponding to |L_j(new_y)|.
     """ 
     def setUp(self):
-        #self.options = helper.dummyOptions()
-        #self.values = helper.dummyValues()
         self.abs_tol=1e-15;
         self.rel_tol=1e-15;
         pass
@@ -41,9 +39,7 @@ class Test_bcdfo_find_new_yj(unittest.TestCase):
 
         correctynew = array([[3.280776988023534, -0.959774286524791]]).T
         correctimprovement =  314.8805392927235
-#        print "ynew", ynew
-#        print "improvement", improvement
-        self.assertAlmostEqual(correctimprovement, improvement, 11)
+        self.assertAlmostEqual(correctimprovement, double(improvement), 11)
         self.assertTrue(compare_array(correctynew, ynew, self.abs_tol, self.rel_tol))
         
         #Same test as above but without the shifting in the interpolation points
@@ -54,47 +50,8 @@ class Test_bcdfo_find_new_yj(unittest.TestCase):
 
         correctynew = array([[3.280776988023534, -0.959774286524791]]).T
         correctimprovement =  314.8805392927235
-#        print "ynew", ynew
-#        print "improvement", improvement
-        
-        self.assertAlmostEqual(correctimprovement, improvement, places=11)
+        self.assertAlmostEqual(correctimprovement, double(improvement), places=11)
         self.assertTrue(compare_array(correctynew, ynew, self.abs_tol, self.rel_tol))
-  
-  
-# Not sure where this example comes from
-#          #changed example to reproduce results from MATLAB, due to optimization
-#        Y = array([[ 3.0, 1.0, 1.0, 2.0, 1.0, 0.0],[0.0, 0.0, 1.0, 0.0, 0.01, 2.0 ]])
-#        whichmodel = 0
-#        QZ, RZ, xbase, scale = bcdfo_build_QR_of_Y_( Y , whichmodel, 1, 1,1, 1e15 )
-#        ynew, improvement = bcdfo_find_new_yj_( QZ, RZ, Y, 5, 1.0, 0.001, xbase, 1, whichmodel, scale, 1 )
-#
-#
-#        #QZ = array(
-#    #[[1.0000,         0,         0,         0,         0,         0],
-#    #[     0,   -0.9636,   -0.0783,   -0.2555,         0,   -0.0000],
-#    # [    0,         0,   -0.7308,    0.2240,    0.5993,    0.2378],
-#    #  [   0,    0.2673,   -0.2823,   -0.9213,         0,   -0.0000],
-#    #   [  0,         0,   -0.1013,    0.0311,   -0.4806,    0.8705],
-#    #    [ 0,         0,    0.6081,   -0.1863,    0.6402 ,   0.4309]])
-#
-#        #RZ = array(
-#
-#   #[ [1.0000,    1.0000,    1.0000,    1.0000,    1.0000,    1.0000],
-#   #  [    0,    0.5756,    0.8943,    0.2775,    0.5756,    0.8943],
-#   #   [   0,         0  , -0.3795,    0.0109,   -0.0030,   -0.7342],
-#   #    [  0,         0 ,        0,    0.0354,    0.0009,    0.1087],
-#   #     [ 0,         0,         0,         0,    0.0007,   -0.0370],
-#   #     [ 0,        0,         0 ,        0,         0 ,   0.0670]])
-#
-#        correctynew = array([3.2866, 0.9581]).T
-#        correctimprovement = 217.2211
-#        #print "ynew", ynew
-#        #print "improvement", improvement
-#        
-#        self.assertAlmostEqual(correctimprovement, improvement, 4)
-#        #print "abs", abs(ynew - correctynew)
-#        self.assertTrue((abs(correctynew - ynew) < 1e-3).all())
-
 
 
 if __name__ == '__main__':
