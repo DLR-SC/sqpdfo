073d7f53bcc43748390ffb9a3569f476f4f83c59
Anke Troeltzsch <anke.troeltzsch@dlr.de>
2018-07-16 16:45:03 +0200

    Bugfix.

--- tests/evalfgh_test.py
+++ tests/evalfgh_test.py
@@ -1,46 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Nov 14 16:28:06 2014
-INPUT VALUES
-key,xy,lm
-
-key =
-
-     2
-
-
-xy =
-
-    -2
-     2
-     2
-     1
-     1
-
-lm = none (Not enough input arguments.)
-OUTPUT VALUES
- outdic,fvalue,info.ci,info.ce
-
-outdic =
-
-     0
-
-
-fvalue =
-
-     3.354626279025119e-04
-
-
-ans =
-
-     []
-
-
-ans =
-
-     4
-    -1
-     1
 
 @author: jaco_da
 """
@@ -50,9 +10,8 @@ import unittest
 from evalfgh import *
 from ecdfo_func import *
 from ecdfo_global_variables import set_prob
-from numpy import array
-#import numpy as np
-#import helper
+from numpy import array, double
+
 
 class Test_evalfgh(unittest.TestCase):
     """
@@ -62,9 +21,6 @@ class Test_evalfgh(unittest.TestCase):
     def setUp(self):
         self.key = 2
         self.xy = array([[-2, 2, 2, 1, 1]]).T
-        #self.options = helper.dummyOptions()
-        #self.values = helper.dummyValues()
-        
         set_simul_not_initialized(0)
         set_prob(5)
 
@@ -74,20 +30,15 @@ class Test_evalfgh(unittest.TestCase):
         """
         msg,out2,out3,out4 = evalfgh_(self.key,self.xy,lm=None)
 
-        #print "msg,out2,out3,out4", msg,out2,out3,out4
-        
         correctmsg = 0
         correctout2 = 3.354626279025119e-04
         correctout4 =    array([4, -1, 1])
         
         self.assertEqual(correctmsg, msg)
-        self.assertAlmostEqual(correctout2, out2, places=15)
+        self.assertAlmostEqual(correctout2, double(out2), places=15)
         self.assertTrue(isempty_(out3))
-        
         self.assertTrue(compare_array(correctout4, out4, 1e-15, 1e-15))
         
-        #self.assertEqual(correctout4, out4)
-
 
 if __name__ == '__main__':
     unittest.main()
