2104adf2d99b088aaa2c310918bdd65cd4b7c54e
Anke Troeltzsch <anke.troeltzsch@dlr.de>
2018-07-17 13:32:29 +0200

    Bugfix.

--- tests/ecdfo_test.py
+++ tests/ecdfo_test.py
@@ -1,84 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Dec 02 17:23:18 2014
------INPUT VALUES-----
-func = 
-
-    @evalfgh
-
-
-x0 =
-
-                   0
-                   0
-   0.500000000000000
-
-
-lm0 =
-
-     []
-
-
-lb =
-
-  -0.500000000000000                   0                -Inf
-
-
-ub =
-
-   Inf   Inf   Inf
-
-
-options = 
-
-           algo_method: 'quasi-Newton'
-    algo_globalization: 'trust regions'
-           hess_approx: 'model'
-          bfgs_restart: 0
-          algo_descent: 'powell'
-                   tol: [1.000000000000000e-05 1.000000000000000e-05 1.000000000000000e-05]
-                 dxmin: 1.000000000000000e-06
-                 miter: 500
-                msimul: 500
-               verbose: 2
-                                                            
-                                                            
------OUTPUT VALUES-----
-
-K>> x,lm,info
-
-x =
-
-  -0.500000000000000
-                   0
-   0.500000000000000
-
-
-lm =
-
-                   0
-  -0.000005713064576
-                   0
-   1.999997749517402
-  -0.999996152071198
-
-
-info = 
-
-         g: [3x1 double]
-        ai: []
-        ae: [2x3 double]
-        hl: []
-     niter: 4
-      flag: 0
-    nsimul: [0 11 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
-        ci: []
-        ce: [2x1 double]
-         f: 0.500000000000000
-      glag: [3x1 double]
-     glagn: 6.661338147750939e-16
-     feasn: 2.220446049250313e-16
-     compl: 0
 
 @author: jaco_da
 """
@@ -92,18 +14,18 @@ from ecdfo_global_variables import set_prob, set_threshold, set_fileoutput,set_s
 import numpy as np
 from numpy import array
 
+
 class myOptions():
     def __init__(self):
-        self.algo_method = 'quasi-newton'
-        self.algo_globalization = 'trust regions'
         self.hess_approx = 'model'
         self.bfgs_restart = 0
         self.algo_descent = 'powell'
-        self.tol = array( [1.000000000000000e-05, 1.000000000000000e-05, 1.000000000000000e-05])
-        self.dxmin = 1.000000000000000e-06
+        self.tol = array( [1.000000000000000e-06, 1.000000000000000e-06, 1.000000000000000e-06])
+        self.dxmin = 1.000000000000000e-16
         self.miter = 500
         self.msimul = 500
-        self.verbose = 2
+        self.verbose = 0
+
 
 class Test_ecdfo(unittest.TestCase):
     """
@@ -122,36 +44,34 @@ class Test_ecdfo(unittest.TestCase):
         self.lm0 = array( [])
         self.lb = array([[ -0.500000000000000, 0, -np.Inf]]).T
         self.ub = array([[np.Inf,   np.Inf,   np.Inf]]).T
-        
-        self.options = myOptions()#helper.dummyOptions()
-        #self.values = helper.dummyValues()
-        
-        self.abs_tol=1e-5
-        self.rel_tol=1e-9
+        self.options = myOptions()
+        self.abs_tol=1e-13
+        self.rel_tol=1e-13
 
         pass
 
     def test_ecdfo(self):
         """
          Test which compare python and matlab results
+         test runs with whichmodel = 0 in ecdfo.py
         """
         x,lm,info = ecdfo_(self.func,self.x0,self.lm0,self.lb,self.ub,self.options) 
         correctx = array([[ -0.500000000000000, 0,0.500000000000000]])
-        correctlm = array([[0,-0.000005713064576,0,1.999997749517402,-0.999996152071198]])
-        self.assertTrue(compare_array(correctx, x,self.abs_tol, self.rel_tol)) 
-        self.assertTrue(compare_array(correctlm, lm,self.abs_tol, self.rel_tol))
+        correctlm = array([[0,-0.000005713064576,0,2.,-1.]])
 
+        self.assertTrue(compare_array(correctx, x,self.abs_tol, self.rel_tol)) 
+        self.assertTrue(compare_array(correctlm, lm, 1e-5, 1e-5))
         self.assertEqual(info.niter,4)
         self.assertEqual(info.flag,0)
         self.assertTrue(compare_array(info.f,array([0.5]), self.abs_tol, self.rel_tol))
-        self.assertTrue(compare_array(info.g, array([-1.000001597463464,2.676871464447451e-07,9.999907066947283e-01]),self.abs_tol, 1e-7))
-        self.assertTrue(compare_array(info.ae, array([[1,1,1],[1,2,3]]),self.abs_tol, self.rel_tol))
-        self.assertTrue(compare_array(info.nsimul, array([0,11,0,0,0,0,0,0,0,0,0,0,0,0,0,0]),self.abs_tol, self.rel_tol))
+        self.assertTrue(compare_array(info.g, array([-1.,0.,1.]),1e-5, 1e-5))
+        self.assertTrue(compare_array(info.ae, array([[1.,1.,1.],[1.,2.,3.]]), 1e-11, 1e-11))
+        self.assertTrue(compare_array(info.nsimul, array([0,11,0,0]),self.abs_tol, self.rel_tol))
         self.assertTrue(isempty_(info.hl))
         self.assertAlmostEqual(2.220446049250313e-16,info.feasn,places=15)
         self.assertTrue(compare_array(info.ce, array([2.220446049250313e-16,-1.110223024625157e-16]),self.abs_tol, self.rel_tol))
-        self.assertTrue(compare_array(info.glag, array([6.661338147750939e-16,6.617876887227321e-16,-5.551115123125783e-16]), self.abs_tol, self.rel_tol))
-        self.assertAlmostEqual(6.661338147750939e-16,info.glagn,places=8)
+        self.assertTrue(compare_array(info.glag, array([0.,0.,0.]), 1e-9, 1e-9))
+        self.assertAlmostEqual(0.,info.glagn,places=8)
         
 
 if __name__ == '__main__':
