7e13f9ea11e2aa56da535beed196a930bb4267bb
Anke Troeltzsch <anke.troeltzsch@dlr.de>
2019-08-15 16:34:15 +0200

    modified for handling inequalities

--- ecdfo_computeHessian.py
+++ ecdfo_computeHessian.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from runtime import *
+import ecdfo_global_variables as glob
 from bcdfo_computeP import bcdfo_computeP_
 from bcdfo_hessP import bcdfo_hessP_
 from ecdfo_bfgs_update import ecdfo_bfgs_update_
 from copy import copy
-from numpy import zeros
+from numpy import zeros, ones, concatenate
 
-def ecdfo_computeHessian_(simul=None,x=None,null_step=None,constrained_pbl=None,lm=None,M=None,n=None,me=None,mi=None,s=None,gx=None,gci=None,gce=None,info_=None,options=None,values=None,fcmodel=None,Y=None,fY=None,ciY=None,ceY=None,sigma=None,scale=None,shift_Y=None,QZ=None,RZ=None,whichmodel=None,ind_Y=None,i_xbest=None,m=None,*args,**kwargs):
+def ecdfo_computeHessian_(simul=None,x=None,null_step=None,constrained_pbl=None,\
+    lm=None,M=None,n=None,me=None,mi=None,s=None,gx=None,gci=None,gce=None,\
+    info_=None,options=None,values=None,fcmodel=None,Y=None,fY=None,ciY=None,\
+    ceY=None,sigma=None,scale=None,shift_Y=None,QZ=None,RZ=None,\
+    whichmodel=None,ind_Y=None,i_xbest=None,m=None,*args,**kwargs):
 
 ###############################################################################
 # approximation of the Hessian of the Lagrangian
 ###############################################################################
 
     info=copy(info_)
+    nbr_slacks = glob.get_nbr_slacks()
+    sl = glob.get_slacks()
 
     lagfY=zeros(size_(Y,2))
     norm_ceY = zeros(size_(Y,2))
@@ -73,22 +80,33 @@ def ecdfo_computeHessian_(simul=None,x=None,null_step=None,constrained_pbl=None,
             cur_degree=size_(Y,2)
 
             # constrained case
+            
             if not isempty_(ceY):
 
                 #  Compute Lagrange function values for all points
-                for i in range(0,cur_degree):
-                    norm_ceY[i] = norm_(ceY[:,i])
-                    norm_lm[i] = lm[n:n+me].T.dot(ceY[:,i])
                 
+                if nbr_slacks > 0:
+                    for i in range(0,cur_degree):
+                        norm_ceY[i] = norm_(ceY[:,i] - \
+                                   concatenate((zeros((me-nbr_slacks,1)),sl**2)).T[0])   
+                        norm_lm[i] = lm[n:n+me].T.dot(\
+                                   concatenate((zeros((me-nbr_slacks,1)),sl**2)).T[0])
+                else:
+                    for i in range(0,cur_degree):
+                        norm_ceY[i] = norm_(ceY[:,i])
+                        norm_lm[i] = lm[n:n+me].T.dot(ceY[:,i])
+
                 lagfY = fY + sigma * norm_ceY + norm_lm
 
                 # Compute model of the Lagrangian
+                
                 model=\
                 bcdfo_computeP_(QZ,RZ,Y,lagfY.reshape(1,-1),\
                                whichmodel,fcmodel[[0],:],ind_Y,\
                                i_xbest,m,gx,scale,shift_Y)
                 
                 # Compute Hessian of the Lagrangian
+                
                 M=bcdfo_hessP_(model,x,x,scale,shift_Y)
                 
             else:
--- ecdfo_optimality.py
+++ ecdfo_optimality.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from runtime import *
+import ecdfo_global_variables as glob
 from copy import copy
-from numpy import arange
-from numpy import logical_and
+from numpy import arange, logical_and, zeros, diag, ones, concatenate
 
 
 def ecdfo_optimality_(x=None,lm=None,lb=None,ub=None,info_=None,options=None,*args,**kwargs):
@@ -13,6 +13,8 @@ def ecdfo_optimality_(x=None,lm=None,lb=None,ub=None,info_=None,options=None,*ar
     ##############################################################
 
     info=copy(info_)
+    nbr_slacks = glob.get_nbr_slacks()
+    sl = glob.get_slacks()
 
     # Dimensions
 
@@ -23,7 +25,6 @@ def ecdfo_optimality_(x=None,lm=None,lb=None,ub=None,info_=None,options=None,*ar
     # Gradient of the Lagrangian
 
     info.glag=copy(info.g)
-    gradlag=info.g
 
     # consider only active and violated bounds for gradient computation
     bounds=(abs(lb - x) < 1e-05) + (abs(ub - x) < 1e-05)
@@ -33,30 +34,29 @@ def ecdfo_optimality_(x=None,lm=None,lb=None,ub=None,info_=None,options=None,*ar
     if options.verbose >= 4:
         fprintf_('\n     lb             x            ub             glag            lm\n')
         for i in range(0,n):
-            fprintf_('%12.5e  %12.5e  %12.5e  %12.5e  %12.5e\n'%(lb[i],x[i],ub[i],info.glag[i],lm[i]))
+            fprintf_('%12.5e  %12.5e  %12.5e  %12.5e  %12.5e\n'%(lb[i],x[i],ub[i],\
+            info.glag[i],lm[i]))
     I=find_(bounds[0:n])
     info.glag[I]=info.glag[I] + lm[I]
-    boundsmult=lm[I]
-    gradlag=info.glag
-
-    # contribution of the inequality constraints
-
-    if mi > 0:
-        I=find_(bounds[n:n + mi])
-        info.glag=info.glag + info.ai(I,arange()).T.dot(lm[n + I])
 
     # contribution of the equality constraints
     
     if me > 0:
-        derivequcons=info.ae
-        equmult=lm[n + mi:n + mi + me]
-        info.glag=info.glag + info.ae.T.dot(lm[n + mi:n + mi + me])
-        gradlag=info.glag
-        
+        if nbr_slacks > 0:
+            hh1 = concatenate((zeros((me-nbr_slacks,nbr_slacks)),-2*diag(sl.T[0])))
+            gconstraints = concatenate((info.ae,hh1),axis=1)
+            glag = concatenate((info.glag,zeros((nbr_slacks,1))))
+            glag = glag + gconstraints.T.dot(lm[n:n+me])
+            info.glag=glag[1:n]
+        else:
+            info.glag=info.glag + info.ae.T.dot(lm[n + mi:n + mi + me])
+                    
     # Feasibility (a vector)
 
-    feas=concatenate_([max_(0,max_(concatenate_([x, info.ci]) - ub, lb - concatenate_([x,info.ci]))) ,
-										info.ce])
+    if nbr_slacks > 0:
+        feas = info.ce - concatenate((zeros((me-nbr_slacks,1)),sl**2))
+    else:
+        feas = info.ce
 
     # Complementarity (a vector)
 
@@ -64,14 +64,16 @@ def ecdfo_optimality_(x=None,lm=None,lb=None,ub=None,info_=None,options=None,*ar
     compl=zeros_(n + mi,1)
 
     # lower bounds on v
-
-    I=find_(logical_and(lb > - options.inf,abs(lb - v) > options.dxmin)) #v(I) is inactive at its lower bounds
+    
+    #v(I) is inactive at its lower bounds
+    I=find_(logical_and(lb > - options.inf,abs(lb - v) > options.dxmin)) 
     if not isempty_(I):  
         compl[I]=max_(compl[I],max_(0,- lm[I])) # lm(I) must be >= 0
 
     # upper bounds on v
 
-    I=find_(logical_and(ub < options.inf,abs(ub - v) > options.dxmin)) # v(I) is inactive at its upper bounds
+    # v(I) is inactive at its upper bounds
+    I=find_(logical_and(ub < options.inf,abs(ub - v) > options.dxmin)) 
     if not isempty_(I):  
         compl[I]=max_(compl[I],max_(0,lm[I])) # lm(I) must be <= 0
 
